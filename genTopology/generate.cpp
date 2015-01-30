4910d32b30d126e606c3292652f093f3594f93b7
yanxiangtianji <yanxiangtianji@gmail.com>
2015-01-29 23:34:01 -0500

    function group of "gen_tp_degree"

--- genTopology/generate.cpp
+++ genTopology/generate.cpp
@@ -1,34 +1,72 @@
 #include "generate.h"
+#include <random>
+#include <algorithm>
+#include <iterator>
 
 using namespace std;
 
-AdjGraph gen_topology(const size_t n_node, std::function<size_t(const size_t nid)> outdegree_n_fun,
-	std::function<bool(const size_t from, const size_t to)> link_pred_fun)
+// Generate link by using $fun_prob_link$ on each node pair.
+AdjGraph gen_tp_prob(const bool selfloop, const size_t n_node,
+	std::function<bool(const size_t from, const size_t to)> fun_prob_link)
 {
 	return AdjGraph(1);
 }
 
 // Generate $n_edge[i]$ edges for each node $i$, randomly connect to other nodes.
-AdjGraph gen_topology(const bool selfloop, const size_t n_node, const std::vector<size_t>& n_edges){
+AdjGraph gen_tp_degree(const bool selfloop, const size_t n_node, const std::vector<size_t>& n_edges){
+	random_device rd;
+	mt19937 gen(rd());
+	uniform_int_distribution<size_t> dist(0, n_node);
+	const size_t n = n_node - selfloop ? 0 : 1;
 
+	vector<size_t> sf_vec;
+	vector<bool> used;
+	AdjGraph g(n_node);
+	for(size_t i = 0; i < n_node; ++i){
+		size_t m = n_edges[i];
+		if(m <= n_node << 1){	//mark and re-generate
+			used.assign(n_node, false);
+			used[i] = true;
+			for(size_t j = 0; j < m; j++){
+				size_t t = dist(gen);
+				while(used[t])
+					t = dist(gen);
+				used[t] = true;
+				g.add(i, t);
+			}
+		}else{	//shuffle a continuous list and pick the first m
+			if(sf_vec.empty()){//first use the shuffle vector
+				sf_vec.reserve(n_node);
+				size_t t = 0;
+				generate_n(back_inserter(sf_vec), n_node, [&t](){return t++; });
+//				for(size_t i = 0; i < n_node; ++i)
+//					sf_vec.push_back(i);
+			}
+			random_shuffle(sf_vec.begin(), sf_vec.end());
+			for(size_t j = 0; j < m; ++j)
+				g.add(i, j);
+		}
+	}
+	return g;
 }
 
 
 // Generate out-degree by $fun_out_degree$, randomly connect to other nodes.
-AdjGraph gen_topology(const bool selfloop, const size_t n_node,
+AdjGraph gen_tp_degree(const bool selfloop, const size_t n_node,
 	std::function<size_t(const size_t nid)> fun_out_degree)
 {
 	vector<size_t> v;
 	v.reserve(n_node);
 	for (size_t i = 0; i < n_node; ++i)
 		v.push_back(fun_out_degree(i));
-	return gen_topology(selfloop, n_node, v);
+	return gen_tp_degree(selfloop, n_node, v);
 }
 
 // Generate out-degree by $n_edge$/$fun_out_degree(i)$, randomly connect to other nodes.
-AdjGraph gen_topology(const bool selfloop, const size_t n_node, const size_t n_edge,
+AdjGraph gen_tp_degree(const bool selfloop, const size_t n_node, const size_t n_edge,
 	std::function<double(const size_t nid)> fun_out_degree)
 {
+	const size_t max_od = n_node - (selfloop?0:1);
 	double sum = 0.0;
 	vector<double> temp;
 	temp.reserve(n_node);
@@ -41,11 +79,12 @@ AdjGraph gen_topology(const bool selfloop, const size_t n_node, const size_t n_e
 	v.reserve(n_node);
 	size_t n_sum = 0;
 	for(size_t i = 0; i < n_node-1; ++i){
-		size_t t = round(n_edge / temp[i]);
+		size_t t = static_cast<size_t>(round(n_edge / temp[i]));
+		t = t>max_od ? max_od : t;
 		v.push_back(t);
 		n_sum += t;
 	}
 	v.push_back(n_edge - n_sum);
-	return gen_topology(selfloop, n_node, v);
+	return gen_tp_degree(selfloop, n_node, v);
 
 }
--- genTopology/generate.h
+++ genTopology/generate.h
@@ -2,18 +2,21 @@
 #include <random>
 #include "AdjGraph.h"
 
-
-AdjGraph gen_topology(const bool selfloop, const size_t n_node,
-	std::function<size_t(const size_t nid)> fun_out_degree,
+// Generate link by using $fun_prob_link$ on each node pair.
+AdjGraph gen_tp_prob(const bool selfloop, const size_t n_node,
 	std::function<bool(const size_t from, const size_t to)> fun_prob_link);
 
+
 // Generate $n_edge[i]$ edges for each node $i$, randomly connect to other nodes.
-AdjGraph gen_topology(const bool selfloop, const size_t n_node, const std::vector<size_t>& n_edges);
+AdjGraph gen_tp_degree(const bool selfloop, const size_t n_node, const std::vector<size_t>& n_edges);
 
 // Generate out-degree by $fun_out_degree$, randomly connect to other nodes.
-AdjGraph gen_topology(const bool selfloop, const size_t n_node,
+AdjGraph gen_tp_degree(const bool selfloop, const size_t n_node,
 	std::function<size_t(const size_t nid)> fun_out_degree);
 
 // Generate out-degree by $n_edge$/$fun_out_degree(i)$, randomly connect to other nodes.
-AdjGraph gen_topology(const bool selfloop, const size_t n_node, const size_t n_edge,
+/*!
+output of $fun_out_degree$ should be in [0,+inf)
+*/
+AdjGraph gen_tp_degree(const bool selfloop, const size_t n_node, const size_t n_edge,
 	std::function<double(const size_t nid)> fun_out_degree);
