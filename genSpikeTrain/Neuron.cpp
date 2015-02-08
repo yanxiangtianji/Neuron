53aaffeb11bc7a45daf63e79ee535ac309280d61
yanxiangtianji <yanxiangtianji@gmail.com>
2015-02-08 00:54:57 -0500

    1, change some type/function name. 2, fix delay functions. 3, add more neuron dynamic details(shreshold, receive amount, fire_interval). 4, default version of some dynamics. 5, comparision. 6, comments

--- genSpikeTrain/Neuron.cpp
+++ genSpikeTrain/Neuron.cpp
@@ -1,19 +1,28 @@
 #include "Neuron.h"
 
+tp_t Neuron::fire_min_interval=0;
 
-Neuron::Neuron(const nid_t nid, fun_delay_t f) :id(nid), fun_delay(f)
+Neuron::fun_delay_t Neuron::default_fun_delay = [](const tp_t& m){return m; };
+Neuron::cb_fire_t Neuron::default_cb_fire = [](neu_ptr_t p, const tp_t& t){p->receive(t); };
+
+
+Neuron::Neuron(const nid_t nid, const signal_t fire_shd, const tp_t fire_d, fun_delay_t fire_f, fun_delay_t prog_f)
+	:id(nid), fire_shreshold(fire_shd), fire_delay(fire_d), fun_delay_f(fire_f), fun_delay_p(prog_f)
 {
 }
 
-void Neuron::fire(const tp_t tp){
+void Neuron::fire(const tp_t current){
 	for(auto p : children){
-		tp_t t = fun_delay(p.second);
-		cb_fire(p.first, t);
+		tp_t delay = fun_delay_p(p.second);
+		cb_fire(p.first, current + delay);
 	}
 }
 
 
-void Neuron::get(const tp_t tp){
-	fire(tp);
+void Neuron::receive(const tp_t current){
+	if(last_fire_time + fire_min_interval < current){
+		last_fire_time = current;
+		fire(current + fun_delay_f(fire_delay));
+	}
 }
 
--- genSpikeTrain/Neuron.h
+++ genSpikeTrain/Neuron.h
@@ -7,30 +7,71 @@
 
 class Neuron
 {
-public:	//typedef
+public:	//for typedef
+	typedef float signal_t;
 	typedef std::shared_ptr<Neuron> ptr_t;
-	typedef std::function<tp_t(const tp_t)> fun_delay_t;
+	typedef std::function<tp_t(const tp_t&)> fun_delay_t;
+	typedef std::function<void(ptr_t, const tp_t&)> cb_fire_t;
 public:
-	Neuron(const nid_t nid);
-	Neuron(const nid_t nid, fun_delay_t f);
+//	Neuron(const nid_t nid);
+	Neuron(const nid_t nid, const signal_t fire_shd = 0, const tp_t fire_d = 0,
+		fun_delay_t fire_f = default_fun_delay, fun_delay_t prog_f = default_fun_delay);
 	Neuron(const Neuron&) = default;
 
 	void add_child(ptr_t& p, const tp_t t){ children[p] = t; }
-	void get_delay_fun(fun_delay_t f){ fun_delay = f; }
 
-	void fire(const tp_t tp);
-	void get(const tp_t tp);
+	/*!
+	@brief Handle the things when this neuron fires.
+	@details Call $cb_fire$ for each of its child, with proper delay.
+		default $cb_fire$ calls the receive function of its each child.
+	@param current The time at which it fires.
+	*/
+	void fire(const tp_t current);
+	/*!
+	@brief Handle the input.
+	@param current The time at which it receives a signal.
+	@param amount The amount of signal the neuron actually receives.
+	*/
+	void receive(const tp_t current, const signal_t& amount);
+	void receive(const tp_t current);
 	void reg(std::function<void(ptr_t, tp_t)> f){ cb_fire = f; }
 
+//getter & setter:
 	nid_t get_id()const { return id; }
+	tp_t get_last_fire_time()const { return last_fire_time; }
+	tp_t get_fire_delay()const { return fire_delay; }
+	signal_t get_fire_shreshold()const { return fire_shreshold; }
+	fun_delay_t get_fun_delay_f()const{ return fun_delay_f; }
+	fun_delay_t get_fun_delay_p()const{ return fun_delay_p; }
+	void set_fire_shreshold(const signal_t& t){ fire_shreshold = t; }
+	void set_fire_delay(const tp_t& d){ fire_delay = d; }
+	void set_fun_delay_p(const fun_delay_t& f){ fun_delay_p = f; }
+	void set_fun_delay_f(const fun_delay_t& f){ fun_delay_f = f; }
 private:
-//	float delay;
 	const nid_t id;
-	std::map<ptr_t, tp_t> children;//child neuron and the delay to it.
-	fun_delay_t fun_delay;
+	signal_t fire_shreshold;
+//	signal_t value;
+	//delays:
+	tp_t fire_delay;	//mean value of fire delay
+	fun_delay_t fun_delay_f;
+	std::map<ptr_t, tp_t> children;//child neuron and the propagation delay to it.
+	fun_delay_t fun_delay_p;
 
-	std::function<void(ptr_t, tp_t)> cb_fire;
+	cb_fire_t cb_fire;
+	tp_t last_fire_time;
+//static:
+	static tp_t fire_min_interval;
+	static fun_delay_t default_fun_delay;
+	static cb_fire_t default_cb_fire;
 };
 
 typedef Neuron::ptr_t neu_ptr_t;
+typedef Neuron::signal_t neu_signal_t;
 
+inline bool operator<(const Neuron& lth, const Neuron& rth){
+	return lth.get_id() < rth.get_id();
+}
+
+inline bool operator==(const Neuron& lth, const Neuron& rth){
+	return lth.get_id() == rth.get_id();
+}
