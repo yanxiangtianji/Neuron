f9d87b67d77a6f63a3e6fc6d0c54f91ff53d9b0e
yanxiangtianji <yanxiangtianji@gmail.com>
2015-02-14 23:52:12 -0500

    parent/child version of add & remove functions

--- genTopology/AdjGraph.h
+++ genTopology/AdjGraph.h
@@ -12,10 +12,16 @@ public:
 	bool add(const size_t from, const size_t to);
 	bool remove(const size_t from, const size_t to);
 
+	bool add_parent(const size_t chd, const size_t pnt){ return add(pnt, chd); }
+	bool add_child(const size_t pnt, const size_t chd){ return add(pnt, chd); }
+	bool remove_parent(const size_t chd, const size_t pnt){ return remove(pnt, chd); }
+	bool remove_child(const size_t pnt, const size_t chd){ return remove(pnt, chd); }
+
 	AdjGraph& sort_up();
 	std::ostream& output(std::ostream& os) const;
 
 private:
+	//store each node's children
 	std::vector<std::vector<size_t> > cont;
 	bool unsorted_any;
 	std::vector<bool> unsorted;
