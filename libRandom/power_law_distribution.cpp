9deec306224ce5270a036e6df7f565abe8cd6b07
yanxiangtianji <yanxiangtianji@gmail.com>
2015-02-19 22:55:18 -0500

    lib project: random

---
+++ libRandom/power_law_distribution.cpp
@@ -0,0 +1,11 @@
+#include "power_law_distribution.h"
+
+
+power_law_distribution::power_law_distribution()
+{
+}
+
+
+power_law_distribution::~power_law_distribution()
+{
+}
---
+++ libRandom/power_law_distribution.h
@@ -0,0 +1,10 @@
+#pragma once
+
+
+class power_law_distribution
+{
+public:
+	power_law_distribution();
+	~power_law_distribution();
+};
+
