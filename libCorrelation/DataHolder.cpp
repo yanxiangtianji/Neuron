71dfce9d670deceb7f46f838abf6071f0082cbbe
yanxiangtianji <yanxiangtianji@gmail.com>
2015-03-06 00:43:16 -0500

    typo, remove member spike-train storage, new constructor with spike-count-vector

--- libCorrelation/DataHolder.cpp
+++ libCorrelation/DataHolder.cpp
@@ -3,33 +3,38 @@
 using namespace std;
 
 DataHolder::DataHolder(const tp_t window_size, const tp_t start, const tp_t end, const string& fn)
-	:window_size(window_size), start_t(start), end_t(end), sts(fn)
+	:window_size(window_size), start_t(start), end_t(end)
 {
-	_init(sts);
+	SpikeTrains sts(fn);
+	_init(move(sts));
 }
 
 DataHolder::DataHolder(const tp_t window_size, const tp_t start, const tp_t end, const SpikeTrains& sts)
-	: window_size(window_size), start_t(start), end_t(end), sts(sts)
+	: window_size(window_size), start_t(start), end_t(end)
 {
 	_init(sts);
 }
 
-DataHolder::DataHolder(const tp_t window_size, const tp_t start, const tp_t end, SpikeTrains&& sts)
-	: window_size(window_size), start_t(start), end_t(end), sts(sts)
+DataHolder::DataHolder(const tp_t window_size, const tp_t start, const tp_t end, const std::vector<SCV>& cont)
+	: window_size(window_size), start_t(start), end_t(end), cont(cont)
 {
-	_init(sts);
+}
+DataHolder::DataHolder(const tp_t window_size, const tp_t start, const tp_t end, std::vector<SCV>&& cont)
+	: window_size(window_size), start_t(start), end_t(end), cont(cont)
+{
 }
 
 void DataHolder::_init(const SpikeTrains& sts){
+	cont.reserve(sts.size());
 	for(const SpikeTrains::SpikeTrain& st : sts.get()){
 		cont.push_back(SCV(window_size, start_t, end_t, st));
 	}
 	cont.shrink_to_fit();
 }
 
-double DataHolder::pearson_correlatino(const SCV& first, const SCV& second){
+double DataHolder::pearson_correlation(const SCV& first, const SCV& second){
 	size_t l = first.get_length();
-	int sx1 = 0, sx2 = 0, sy1 = 0, sy2 = 0, xy = 0;
+	SCV::value_type sx1 = 0, sx2 = 0, sy1 = 0, sy2 = 0, xy = 0;
 	for(size_t i = 0; i < l; ++i){
 		int x = first[i];
 		sx1 += x;
@@ -39,16 +44,16 @@ double DataHolder::pearson_correlatino(const SCV& first, const SCV& second){
 		sy2 += y*y;
 		xy += x*y;
 	}
-	int up = l*xy - sx1*sy1;
-	int down2 = (l*sx2 - sx1*sx1)*(l*sy2 - sy1*sy1);
+	SCV::value_type up = l*xy - sx1*sy1;
+	SCV::value_type down2 = (l*sx2 - sx1*sx1)*(l*sy2 - sy1*sy1);
 	return up / sqrt(down2);
 }
-double DataHolder::pearson_correlatino(const size_t first, const size_t second){
-	return pearson_correlatino(cont[first], cont[second]);
+double DataHolder::pearson_correlation(const size_t first, const size_t second){
+	return pearson_correlation(cont[first], cont[second]);
 }
-double DataHolder::pearson_correlatino(const size_t first, const std::vector<size_t>& second){
+double DataHolder::pearson_correlation(const size_t first, const std::vector<size_t>& second){
 	std::vector<reference_wrapper<const SCV>> temp;
 	for(size_t idx : second)
 		temp.push_back(cref(cont[idx]));
-	return pearson_correlatino(cont[first], SCV::union_v(temp));
+	return pearson_correlation(cont[first], SCV::union_v(temp));
 }
--- libCorrelation/DataHolder.h
+++ libCorrelation/DataHolder.h
@@ -10,17 +10,20 @@ class DataHolder
 public:
 	DataHolder(const tp_t window_size, const tp_t start, const tp_t end, const std::string& fn);
 	DataHolder(const tp_t window_size, const tp_t start, const tp_t end, const SpikeTrains& sts);
-	DataHolder(const tp_t window_size, const tp_t start, const tp_t end, SpikeTrains&& sts);
+	DataHolder(const tp_t window_size, const tp_t start, const tp_t end, const std::vector<SCV>& cont);
+	DataHolder(const tp_t window_size, const tp_t start, const tp_t end, std::vector<SCV>&& cont);
 
-	double pearson_correlatino(const SCV& first, const SCV& second);
-	double pearson_correlatino(const size_t first, const size_t second);
-	double pearson_correlatino(const size_t first, const std::vector<size_t>& second);
+	double pearson_correlation(const SCV& first, const SCV& second);
+	double pearson_correlation(const size_t first, const size_t second);
+	double pearson_correlation(const size_t first, const std::vector<size_t>& second);
 
 
 	size_t size()const { return cont.size(); }
 	tp_t get_window_size()const{ return window_size; }
 	tp_t get_start_t()const{ return start_t; }
 	tp_t get_end_t()const{ return end_t; }
+	SCV& operator[](const size_t idx){ return cont[idx]; }
+	const SCV& operator[](const size_t idx)const { return cont[idx]; }
 private:
 	void _init(const SpikeTrains& sts);
 	size_t cal_idx(const tp_t t){ return size_t((t - start_t) / window_size); }
@@ -31,7 +34,6 @@ private:
 	tp_t start_t, end_t;
 
 	std::vector<SCV> cont;
-	SpikeTrains sts;
 
 };
 
