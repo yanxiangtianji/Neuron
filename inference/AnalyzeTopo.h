cd25a3c2624ad8991cdb64944e129d9a12c05422
yanxiangtianji <yanxiangtianji@gmail.com>
2015-03-18 00:21:48 -0400

    1, change type adj_g_t. 2, add function: get_adj_g.

--- inference/AnalyzeTopo.h
+++ inference/AnalyzeTopo.h
@@ -1,10 +1,11 @@
 #pragma once
 #include <vector>
+#include "AlgBase.h"
 
 class AnalyzeTopo
 {
 public:
-	typedef std::vector<std::vector<size_t>> adj_g_t;
+	typedef AlgBase::pss_t adj_g_t;
 	typedef std::vector<std::vector<double>> prob_g_t;
 
 public:
@@ -13,6 +14,7 @@ public:
 
 	void add(const adj_g_t& g);
 	prob_g_t get_prob_g() const;
+	adj_g_t get_adj_g() const{ return count; }
 	size_t get_n_node()const{ return n_node; }
 	size_t get_n_graph()const{ return n_graph; }
 
