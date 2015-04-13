d4a9dde1dd2b4f43f4fe2c70376fa85214876815
yanxiangtianji <yanxiangtianji@gmail.com>
2015-04-13 17:42:31 -0400

    generalized version of test function (outputing Confusion Matrix or list of [tp fn fp tn])

--- mRegression/test.m
+++ mRegression/test.m
@@ -1,17 +1,22 @@
 function [varargout]=test(p,y)
-
+%disp(sprintf("nout=%d",nargout))
+%nargout=max(1,nargout)
 tp=sum(p==1 & y==1);
 fp=sum(p==1 & y==0);
 fn=sum(p==0 & y==1);
 tn=sum(p==0 & y==0);
 
-if(nargout==1)
-  varargout=mat2cell([tp,fp,fn,tn],1,4);
-else
+if(nargout<=1)
+  varargout=mat2cell([tp,fn,fp,tn],1,4);
+elseif(nargout>=2)
   varargout(1)=num2cell(tp);
-  varargout(2)=num2cell(fp);
-  varargout(3)=num2cell(fn);
-  varargout(4)=num2cell(tn);
+  varargout(2)=num2cell(fn);
+  if(nargout>=3)
+    varargout(3)=num2cell(fp);
+    if(nargout==4)
+      varargout(4)=num2cell(tn);
+    end
+  end
 end
 
 end
