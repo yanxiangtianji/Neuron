f403b3985826bdd6a6b1b7551c4295beb4aa535d
yanxiangtianji <yanxiangtianji@gmail.com>
2015-04-30 23:58:54 -0400

    distribution for all. add new last entry of cue statistics cell array to represent statistics over all.

--- mRegression/_snippet.m
+++ mRegression/_snippet.m
@@ -3,10 +3,10 @@
 
 function [Aarr,Warr,CMarr,SCarr]=whole_list_AW(fn_list,n_trial,n,D,Ainit,Winit,lambdaA,lambdaW)
   n_cue=length(fn_list);
-  Aarr=cell(1,n_trial);   #Adjacent (n*n)
-  Warr=cell(1,n_trial);   #Weight (n*n)
-  CMarr=cell(1,n_trial);  #Confusion Matrix (n*4)
-  SCarr=cell(1,n_trial); #Spike Count (n)
+  Aarr=cell(n_trial,1);   #Adjacent (n*n)
+  Warr=cell(n_trial,1);   #Weight (n*n)
+  CMarr=cell(n_trial,1);  #Confusion Matrix (n*4)
+  SCarr=cell(n_trial,1); #Spike Count (n)
   for i=1:n_trial
     %disp(fn_list(i))
     %rData=readRawSpike(fn_spike);   dMin=0.0001;    dUnit=0.0001;
@@ -34,13 +34,135 @@ n=19;
 %[Aarr4,Warr4,CMarr4,SCmat4]=whole_list_AW(fn_r2(idx),m,n,D,Ainit,Winit,1,1);
 
 m=40;
-Aarr=cell(4,m);Warr=cell(4,m);CMarr=cell(4,m);SCarr=cell(4,m);
-fnlist=cell(4,m);
-fnlist(1,:)=fn_c1(1:m);fnlist(2,:)=fn_c2(1:m);fnlist(3,:)=fn_r1(1:m);fnlist(4,:)=fn_r2(1:m);
+Aarr=cell(m,4);Warr=cell(m,4);CMarr=cell(m,4);SCarr=cell(m,4);
+fnlist=cell(m,4);
+fnlist(:,1)=fn_c1(1:m);fnlist(:,2)=fn_c2(1:m);fnlist(:,3)=fn_r1(1:m);fnlist(:,4)=fn_r2(1:m);
 for i=1:4
-  tic;[Aarr(i,:),Warr(i,:),CMarr(i,:),SCarr(i,:)]=whole_list_AW(fnlist(i,:),m,n,D,Ainit,Winit,1,1);toc;
+  tic;[Aarr(:,i),Warr(:,i),CMarr(:,i),SCarr(:,i)]=whole_list_AW(fnlist(:,i),m,n,D,Ainit,Winit,1,1);toc;
 end
-save('from_data.mat','D','Ainit','Winit','Aarr','Warr','CMarr','SCarr')
+save('data1.mat','D','Ainit','Winit','Aarr','Warr','CMarr','SCarr')
+
+##################
+#accurancy & valid:
+
+#accurancy:
+function disAcc=distriCMmat_acc(CMarr)
+  l=length(CMarr(:));
+  n=size(cell2mat(CMarr(1)),1);
+  disAcc=zeros(n,l);
+  for i=1:l
+    x=cell2mat(CMarr(i));
+    disAcc(:,i)=(x(:,1)+x(:,4))./sum(x,2);
+  end
+end
+
+AccD=distriCMmat_acc(CMarr(:));
+
+function [avgV,stdV]=statVec(dis)
+  avgV=mean(dis,2);
+  stdV=std(dis,1,2);
+end
+
+AccAvg=zeros(n,5);  AccStd=zeros(n,5);
+for cue_id=1:4;
+  [AccAvg(:,cue_id),AccStd(:,cue_id)]=statVec(AccD(:,m*(cue_id-1)+1:m*cue_id));
+end
+[AccAvg(:,5),AccStd(:,5)]=statVec(AccD);
+
+save('acc.mat','AccD','AccAvg','AccStd');
+
+%statistics of Accurancy
+subplot(2,1,1);imagesc(AccAvg');title('AVG accurancy');caxis([0 1]);colorbar;colormap(gray);
+set(gca,'yticklabel',['dummy';'cue 1'; 'cue 2'; 'rest 1'; 'rest 2'; 'all'])
+set(gca,'xtick',[1:19])
+subplot(2,1,2);imagesc(AccStd');title('STD accurancy');caxis([0 1]);colorbar;colormap(gray);
+set(gca,'yticklabel',['dummy';'cue 1'; 'cue 2'; 'rest 1'; 'rest 2'; 'all'])
+set(gca,'xtick',[1:19])
+
+%distribution of accurancy
+for i=1:19;
+  subplot(4,5,i);hist(AccD(i,:),20);title(['Acc. distr. on N',num2str(i)]);set(gca,'xtick',[0 0.5 1]);
+end
+
+function plotAccDis(AccD)
+  subplot(2,1,1);hist(AccD(:),50);title('Overall accurancy distribution');
+  xlabel('accurancy');ylabel('frequency');
+  subplot(2,1,2);plotcdf(AccD(:),50,'Overall accurancy CDF','accurancy','CDF (fraction of nuerons)');
+end
+#valid:
+
+function [nValid_avg,nValid_std]=statCMmat_vld(CMarr)
+  l=length(CMarr(:));
+  n=size(cell2mat(CMarr(1)),1);
+  sum1=zeros(n,1);
+  for i=1:l
+    x=cell2mat(CMarr(i));
+    t=(x(:,1)+x(:,4))./sum(x,2)>0.5; %binary
+    sum1+=t;    %sum2+=t.^2; is equal to sum1
+  end
+  nValid_avg=sum1/l;
+  nValid_std=sqrt(nValid_avg.*(1-nValid_avg)); %handle float error
+end
+
+function [nValid_avg,nValid_std]=acc2vld(accDis,thrd=0.5)
+  vmat=accDis>=thrd;
+  [nValid_avg,nValid_std]=statVec(vmat,1,2);
+end
+%Va=cell(1,4); Vs=cell(1,4);
+%for i=1:4;  [Va(i),Vs(i)]=statCMmat_vld(CMarr(i,:));    end
+%[Vat,Vst]=statCMmat_vld(CMarr(:));
+%valid_avg_all=[cell2mat(Va), Vat]; valid_std_all=[cell2mat(Vs), Vst];
+
+Va=zeros(n,5); Vs=zeros(n,5);
+for i=1:4;
+  [Va(:,i),Vs(:,i)]=acc2vld(AccD(:,m*(i-1)+1:m*i));
+end
+[Va(:,5),Vs(:,5)]=acc2vld(AccD);
+
+vld_idx=cell(1,5);
+for i=1:5;
+  vld_idx(i)=find(Va(:,i)>0.5)';
+end
+
+save('valid.mat','Va','Vs','vld_idx')
+
+
+#################
+#num of spike:
+
+function dis=distriVec(arr)
+  l=length(arr(:));
+  n=size(cell2mat(arr(1)),1);
+  disAcc=zeros(n,l);
+  for i=1:l
+    dis(:,i)=cell2mat(arr(i));
+  end
+end
+
+Cdis=distriVec(SCarr);
+
+Ca=zeros(n,5);  Cs=zeros(n,5);
+for i=1:4
+  [Ca(:,i),Cs(:,i)]=statVec(Cdis(:,m*(i-1)+1:m*i));
+end
+[Ca(:,5),Cs(:,5)]=statVec(Cdis);
+
+save('count.mat','Cdis','Ca','Cs')
+
+
+%relation between being valid and number of spikes
+function plotValidNCount(Va,Vs,Ca)
+  subplot(3,1,1);imagesc(Va');title('Prob.(AVG) of a neuron being valid');caxis([0 1]);colorbar;colormap(gray);
+  set(gca,'yticklabel',['dummy';'cue 1'; 'cue 2'; 'rest 1'; 'rest 2'; 'all'])
+  set(gca,'xtick',[1:19])
+  subplot(3,1,2);imagesc(Vs');title('STD of a neuron being valid');caxis([0 1]);colorbar;colormap(gray);
+  set(gca,'yticklabel',['dummy';'cue 1'; 'cue 2'; 'rest 1'; 'rest 2'; 'all'])
+  set(gca,'xtick',[1:19])
+  fraction=bsxfun(@rdivide,Ca,sum(Ca));
+  subplot(3,1,3);imagesc(log10(fraction'));title('Log10 of spikes count fraction');colorbar;colormap(gray);
+  set(gca,'yticklabel',['dummy';'cue 1'; 'cue 2'; 'rest 1'; 'rest 2'; 'all'])
+  set(gca,'xtick',[1:19])
+end
 
 #################
 #Adj and weight
@@ -60,13 +182,13 @@ function [avgM,stdM]=statMat(arr)
 end
 
 
-Aa=cell(4,1); As=cell(4,1);
+Aa=cell(1,5); As=cell(1,5);
 for i=1:4;  [Aa(i),As(i)]=statMat(Aarr(i,:));   end
-[Aat,Ast]=statMat(Aarr(:));
-Wa=cell(4,1); Ws=cell(4,1);
+[Aa(5),As(5)]=statMat(Aarr(:));
+Wa=cell(1,5); Ws=cell(1,5);
 for i=1:4;  [Wa(i),Ws(i)]=statMat(Warr(i,:));   end
-[Wat,Wst]=statMat(Warr(:));
-save('statAW.mat','Aa','As','Aat','Ast','Wa','Ws','Wat','Wst')
+[Wa(5),Ws(5)]=statMat(Warr(:));
+save('statAW.mat','Aa','As','Wa','Ws')
 
 for i=1:4;
   temp=cell2mat(Aa(i));
@@ -76,8 +198,8 @@ for i=1:4;
   temp=cell2mat(As(i));
   subplot(2,2,i);imagesc(temp);title(['STD adjacency of cue ',num2str(i)]);caxis([0 1]);colorbar;colormap(gray);
 end
-subplot(2,2,1);imagesc(Aat);title('AVG adjacency overall');caxis([0 1]);colorbar;colormap(gray);
-subplot(2,2,2);imagesc(Ast);title('STD adjacency overall');caxis([0 1]);colorbar;colormap(gray);
+subplot(2,2,1);imagesc(cell2mat(Aa(5)));title('AVG adjacency overall');caxis([0 1]);colorbar;colormap(gray);
+subplot(2,2,2);imagesc(cell2mat(As(5)));title('STD adjacency overall');caxis([0 1]);colorbar;colormap(gray);
 
 for i=1:4;
   temp=cell2mat(Wa(i)); tit='AVG weight of cue ';
@@ -89,25 +211,38 @@ for i=1:4;
   temp=(cell2mat(Aa(i))>0).*temp;   tit='valid STD weight of cue ';
   subplot(2,2,i);imagesc(temp);title([tit,num2str(i)]);caxis([0 1]);colorbar;colormap(gray);
 end
-temp=Wat.*(Aat>0);
+temp=cell2mat(Wa(5)).*(cell2mat(Aa(5))>0);
 subplot(2,2,1);imagesc(temp);title('valid AVG weight overall');caxis([-1 1]);colorbar;colormap(gray);
-temp=Wst.*(Aat>0);
+temp=Ws(5).*(cell2mat(Aa(5))>0);
 subplot(2,2,2);imagesc(temp);title('valid STD weight overall');caxis([0 1]);colorbar;colormap(gray);
 
 #weight statistics:
-x=zeros(1,4); x2=zeros(2,4); %averaged weight 
-y=zeros(1,4); y2=zeros(2,4); %stardard derivation of weight
-for i=1:4
-  temp=cell2mat(Wa(i));
-  x(i)=mean(mean(temp));
-  x2(:,i)=[max(max(temp)); min(min(temp))];
-  temp=cell2mat(Ws(i));
-  y(i)=mean(mean(temp));
-  y2(:,i)=[max(max(temp)); min(min(temp))];
+function showWeightXtrial(Wa,Ws,varargin)
+  l=length(Wa);
+  if(nargin==3)
+    idx=cell2mat(varargin);
+  end
+  x=zeros(1,l); x2=zeros(2,l); %averaged weight 
+  y=zeros(1,l); y2=zeros(2,l); %stardard derivation of weight
+  for i=1:l
+    temp=cell2mat(Wa(i));
+    if(nargin==3)
+      idx_i=cell2mat(idx(i));
+      temp=temp(idx_i,idx_i);
+    end
+    x(i)=mean(mean(temp));
+    x2(:,i)=[max(max(temp)); min(min(temp))];
+    temp=cell2mat(Ws(i));
+    y(i)=mean(mean(temp));
+    y2(:,i)=[max(max(temp)); min(min(temp))];
+  end
+  x   %avg weight on different cues
+  y   %std weight on different cues
+  (y-y2(2,:))./(y2(1,:)-y2(2,:))*100  %normalized std weight
 end
-x   %avg weight on different cues
-y   %std weight on different cues
-(y-y2(2,:))./(y2(1,:)-y2(2,:))*100  %normalized std weight
+
+showWeightXtrial(Wa,Ws);
+showWeightXtrial(Wa,Ws,vld_idx);
 
 
 #pairwise cue difference/similarity
@@ -136,96 +271,29 @@ function dis=distriMat(arr)
   end
 end
 
-Wdis=distriMat(Warr'(:));
+Wdis=distriMat(Warr(:));
 
 dis_idx1=randperm(19,9); dis_idx2=randperm(19,9);
 cue_id=1;
-for i=1:9;
+for i=1:9; %distribution of 9 edges on the same cue
   subplot(3,3,i);hist(Wdis(dis_idx1(i),dis_idx2(i),(cue_id-1)*40+1:cue_id*40));
-  title(['W distrib. ',num2str(dis_idx1(i)),'-',num2str(dis_idx2(i)), ...
-    ', A=',num2str(cell2mat(Aa(cue_id))(dis_idx1(i),dis_idx2(i)))]);
+  title(['W dist. ',num2str(dis_idx1(i)),'-',num2str(dis_idx2(i)), ...
+    ' on cue ',num2str(cue_id),', A=',num2str(cell2mat(Aa(cue_id))(dis_idx1(i),dis_idx2(i)))]);
 end
 
-
-##################
-#valid:
-
-function [valid_prob,valid_prob_std]=statCMmat(CMarr)
-  l=length(CMarr(:));
-  x=cell2mat(CMarr(1));
-  t=(x(:,1)+x(:,4))./sum(x,2)>0.5;
-  sum1=t;
-  sum2=t.^2;
-  for i=2:l
-    x=cell2mat(CMarr(i));
-    t=(x(:,1)+x(:,4))./sum(x,2)>0.5;
-    sum1+=t;
-    sum2+=t.^2;
-  end
-  valid_prob=sum1/l;
-  valid_prob_std=sqrt(max(0,sum2/l-valid_prob.^2)); %handle float error
+for i=1:4; %distribution of 1 edge on 4 cues
+  subplot(2,2,i);hist(Wdis(dis_idx1(i),dis_idx2(i),(cue_id-1)*40+1:cue_id*40));
+  title(['W dist. ',num2str(dis_idx1(i)),'-',num2str(dis_idx2(i)), ...
+    ' on cue ',num2str(cue_id),', A=',num2str(cell2mat(Aa(cue_id))(dis_idx1(i),dis_idx2(i)))]);
 end
 
-%[valid1,vs1]=statCMmat(CMarr1);   [valid2,vs2]=statCMmat(CMarr2);
-%[valid3,vs3]=statCMmat(CMarr3);   [valid4,vs4]=statCMmat(CMarr4);
-%valid=(valid1+valid2+valid3+valid4)/4;
-%vs=(vs1+vs2+vs3+vs4)/4;
-%vidx=valid>0.5;
-%
-%valid_all=[valid1 valid2 valid3 valid4 valid]';
-%vs_all=[vs1 vs2 vs3 vs4 vs]';
-%save('valid.mat','valid1','vs1','valid2','vs2','valid3','vs3','valid4','vs4')
-
-Va=cell(4,1); Vs=cell(4,1);
-for i=1:4;  [Va(i),Vs(i)]=statCMmat(CMarr(i,:));    end
-[Vat,Vst]=statCMmat(CM(:));
-save('valid.mat',Va,Vs,Vat,Vst)
-
-subplot(4,1,1);imagesc(valid_all);title('Prob.(AVG) of a neuron being valid');caxis([0 1]);colorbar;colormap(gray);
-set(gca,'yticklabel',['dummy';'cue 1'; 'cue 2'; 'rest 1'; 'rest 2'; 'all'])
-set(gca,'xtick',[1:19])
-subplot(4,1,2);imagesc(vs_all);title('STD of a neuron being valid');caxis([0 1]);colorbar;colormap(gray);
-set(gca,'yticklabel',['dummy';'cue 1'; 'cue 2'; 'rest 1'; 'rest 2'; 'all'])
-set(gca,'xtick',[1:19])
-
-#valid Weight
-Wav1=Wa1(vidx,vidx);Wav2=Wa2(vidx,vidx);Wav3=Wa3(vidx,vidx);Wav4=Wa4(vidx,vidx);
-Wav=Wa(vidx,vidx);
-Wsv1=Ws1(vidx,vidx);Wsv2=Ws2(vidx,vidx);Wsv3=Ws3(vidx,vidx);Wsv4=Ws4(vidx,vidx);
-Wsv=Ws(vidx,vidx);
-x=[mean(mean(Wav1)),mean(mean(Wav2)),mean(mean(Wav3)),mean(mean(Wav4)),mean(mean(Wav))]
-y=[mean(mean(Wsv1)),mean(mean(Wsv2)),mean(mean(Wsv3)),mean(mean(Wsv4)),mean(mean(Wsv))]
-y2=[max(max(Wav1)),max(max(Wav2)),max(max(Wav3)),max(max(Wav4)),max(max(Wav));
-min(min(Wav1)),min(min(Wav2)),min(min(Wav3)),min(min(Wav4)),min(min(Wav))]
-y3=(y2(1,:)-y2(2,:))
-y./x
-y./y3*100
-
-#################
-#num of spike:
-
-function [avgV,stdV]=statVec(mat)
-  mat=bsxfun(@rdivide,mat,sum(mat,2));
-  l=size(mat,1);
-  avgV=mean(mat);
-  stdV=sqrt(var(mat)*(l-1)/l);
+function plotWeightDis(Wdis,vld_idx)
+  subplot(2,2,1);hist(Wdis(:),100);title('Distribution of all weights');ylabel('frequency');
+  subplot(2,2,2);plotcdf(Wdis(:),50,'CDF of all weights','','CDF');
+  t=Wdis(vld_idx,vld_idx,:)(:);
+  subplot(2,2,3);hist(t,100);title('Distribution of all valid weights');ylabel('frequency');
+  subplot(2,2,4);plotcdf(t,50,'CDF of all weights','','CDF');
 end
+plotWeightDis(Wdis,cell2mat(vld_idx(5)))
 
-[Sa1,Ss1]=statVec(SCmat1);  [Sa2,Ss2]=statVec(SCmat2);
-[Sa3,Ss3]=statVec(SCmat3);  [Sa4,Ss4]=statVec(SCmat4);
-Sa=(Sa1+Sa2+Sa3+Sa4)/4;
-Ss=(Ss1+Ss2+Ss3+Ss4)/4;
 
-Sa_all=[Sa1; Sa2; Sa3; Sa4; Sa];
-Ss_all=[Ss1; Ss2; Ss3; Ss4; Ss];
-
-save('count.mat','Sa1','Ss1','Sa2','Ss2','Sa3','Ss3','Sa3','Ss3','Sa4','Ss4')
-
-%subplot(3,1,2);imagesc(Sa_all);title('AVG fraction of spikes');caxis([0 0.25]);colorbar;colormap(gray);
-subplot(4,1,3);imagesc(log10(Sa_all));title('Log10 of AVG fraction of spikes');colorbar;colormap(gray);
-set(gca,'yticklabel',['dummy';'cue 1'; 'cue 2'; 'rest 1'; 'rest 2'; 'all'])
-set(gca,'xtick',[1:19])
-
-subplot(4,1,4);imagesc(Ss_all);title('STD fraction of spikes');colorbar;colormap(gray);
-set(gca,'yticklabel',['dummy';'cue 1'; 'cue 2'; 'rest 1'; 'rest 2'; 'all'])
-set(gca,'xtick',[1:19])
