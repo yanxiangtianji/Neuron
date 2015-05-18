c38b547b8939e020a2d8c8ea28a46334eff058d9
yanxiangtianji <yanxiangtianji@gmail.com>
2015-05-18 14:45:50 -0400

    remove unnecessary configuration parameters from plotcdf

--- mRegression/plotcdf.m
+++ mRegression/plotcdf.m
@@ -1,7 +1,7 @@
-function plotcdf(x,nsep=20,tit='',xlbl='',ylbl='',xrng='auto')
+function plotcdf(x,nsep=20)
 
 [_h,_x]=hist(x(:),nsep);
-plot(_x,cumsum(_h)/sum(_h),'o-');title(tit);
-xlabel(xlbl);ylabel(ylbl);ylim([0,1]);xlim(xrng);grid;
+plot(_x,cumsum(_h)/sum(_h),'o-');
+ylim([0,1]);grid;
 
 end
