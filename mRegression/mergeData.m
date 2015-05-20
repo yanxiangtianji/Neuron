6508d0a165c8d5cca60db8c6653778e3a7e75c9d
yanxiangtianji <yanxiangtianji@gmail.com>
2015-05-19 23:09:37 -0400

    modify description of mergeData function

--- mRegression/mergeData.m
+++ mRegression/mergeData.m
@@ -1,7 +1,7 @@
 function data=mergeData(varargin)
+%Just concatenate several data piece as row-expansion.
+%varargin is a row vector, varargin(:) converts it into a column vector
 
-%varargin is a raw vector
-%varargin(:) converts it into a column vector
 data=cell2mat(varargin(:));
 
 end
