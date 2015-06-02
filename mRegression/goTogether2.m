293c3e8a8a9bbed1c6f2ac1ac46ed7ce217e80d4
yanxiangtianji <yanxiangtianji@gmail.com>
2015-06-02 00:56:07 -0400

    fix bugs in goTogether2

--- mRegression/goTogether2.m
+++ mRegression/goTogether2.m
@@ -3,7 +3,6 @@ function [Aarr,W,CMarr]=goTogether2(fn_lists,nNeuron,D,fRep,lambdaA,lambdaW,Aini
 %nNeuron:       number of neuron
 %D:             delay matrix
 %fRep:          rate of repolarization
-%alpha:         learning rate in gradient descent
 %lambda(A/W):   regularization parameter for A/W
 %(A/W)init:        initial A/W to start with
 
@@ -16,27 +15,15 @@ function [Aarr,W,CMarr]=goTogether2(fn_lists,nNeuron,D,fRep,lambdaA,lambdaW,Aini
   
   options = optimset('GradObj', 'on', 'MaxIter', 400);
   %main work
-  stopEplison=1e-7;
   for idx=1:nNeuron;
-    Xcell=cell(n,m);
-    ycell=cell(n,m);
-    len=zeros(n,m);
-    for i=1:n; for j=1:m;
-      [Xcell(i,j),t]=genDataFromSnC(nNeuron,cell2mat(seq0s(i,j)),cell2mat(cls0s(i,j)),D,idx,fRep);
-      ycell(i,j)=t;
-      len(i,j)=length(t);
-    end;end;
-    clear t;
     tic;
-    X=cell2mat(Xcell(:));
-    y=cell2mat(ycell(:));
-    len=len(:);
+    [X,y,len]=prepareData(n,m,nNeuron,seq0s,cls0s,idx,D,fRep);
     [t,J]=fminunc(@(t)(costFunctionMASW(X,y,len,t(1:n*m*nNeuron),t(n*m*nNeuron+1:end),lambdaA,lambdaW)),
       [As(:,idx,:)(:);W(:,idx)], options);
-    W(:,idx)=t(n*m*nNeuron+1:end);
     for i=1:n*m;
       As(:,idx,i)=t((i-1)*nNeuron+1:i*nNeuron);
     end;
+    W(:,idx)=t(n*m*nNeuron+1:end);
     toc;
   end;%neuron
 
@@ -70,15 +57,11 @@ function [seq0s,cls0s]=loadDatas(fn_lists)
 end
 
 function [X,y,l]=prepareData(n,m,nNeuron,seq0s,cls0s,idx,D,fRep)
-  [X,y]=genDataFromSnC(nNeuron,cell2mat(seq0s(1,1)),cell2mat(cls0s(1,1)),D,idx,fRep);;
-  l=zeros(n,m);
-  l(1,1)=length(y);
-  for i=1:n; for j=1:m;
-    if(i==1 && j==1)
-      continue;
-    end
+  X=[];y=[];
+  l=zeros(n*m);
+  for j=1:m; for i=1:n; #sets outer iteration to be higher order dismension
     [xt,yt]=genDataFromSnC(nNeuron,cell2mat(seq0s(i,j)),cell2mat(cls0s(i,j)),D,idx,fRep);
     X=[X;xt];  y=[y;yt];
-    l(i,j)=length(yt);
+    l((j-1)*n+i)=length(yt);
   end;end;
 end
