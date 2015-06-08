0dbd3fc3e665ce894f6162406c34e8a176636cbc
yanxiangtianji <yanxiangtianji@gmail.com>
2015-06-07 21:26:23 -0400

    finish mock-ups for hawkes generation function

--- mHawkes/HawkesGenerate.m
+++ mHawkes/HawkesGenerate.m
@@ -18,9 +18,9 @@ function data=HawkesGenerate(n,timeEnd,bkgRate,interRate,delayF)
 %i.e. f(t) = lam*exp(-lam*t)
 %3, Arrival time of n-th event follows Gammar distribution Gammar(n,lam)
 %i.e. f_Tn(t)=lam*exp(-lam*t) * (lam*t)^(n-1) / (n-1)! , t>=0; 0, t<0
-%3.1 joint PDF of t1,t2,...,tn
+%3.1, joint PDF of t1,t2,...,tn
 % f(t1,t2,...,tn)=lam^n * exp(-lam*tn) , t1<t2<...<tn<t; 0, otherwise
-%3.2 joint PDF of t1,t2,...,tn , given N(t)=n
+%3.2, joint PDF of t1,t2,...,tn , given N(t)=n
 % f(t1,t2,...,tn|N(t)=n)=n! / t^n , t1<t2<...<tn<t; 0, otherwise
 
 %Non-Homogeneous Poisson Process (with rate lam(t))
@@ -31,6 +31,8 @@ function data=HawkesGenerate(n,timeEnd,bkgRate,interRate,delayF)
 %i.e. f(t;s) = (lam(s+t)-lam(s))*exp(-(A(s+t)-A(s)))
 %3, Arrival time ~ Gammar(n,A(t))
 %i.e. f_Tn(t) = lam(t)*exp(-A(t)) * (A(t))^(n-1) / (n-1)! , t>=0; 0, t<0
+%3.1, joint PDf of t1,t2,...,tn
+% f(t1,t2,...,tn)=\prod_i^n (lam(ti)) * exp(-A(tn)) , t1<t2<...<tn<t; 0, otherwise
 
 
 %check:
@@ -64,35 +66,48 @@ end
 avgN=timeEnd.*bkgRate;
 rate=bkgRate;
 
-%bfs generation:
-que=zeros(0,2); %time, node
+%layered generation (addible property):
+%first layer (uniformed background rate)
+current=zeros(0,2); %time, node
 closed=zeros(0,2);
-len=avgN;
-%idx=zeros(n,1)+1;
 for i=1:n
   t0=0;
   t=[];
+  len=avgN;
   while(t0<timeEnd)
-    t=[t; t0+cumsum(drawInterval(rate(i),len))];
+    t=[t; t0+cumsum(drawIntervalExp(rate(i),len))];
     t0=t(end);
     len=min(8,len/2);
   end
   t=t(find(t<=TimeEnd));
-  que=[que; [t zeros(length(t),1)+i]];
+  current=[current; [t,zeros(length(t),1)+i]];
 end
-
+closed=current;
+%time-varient rate
+MaxTrig=10;
+while(MaxTrig-->0 && size(que,1)!=0)
+  last=current;
+  current=zeros(0,2);
+  for i=1:n;%idx of node
+    %calculate lam(t) and A(t) of node i under the influenec of last layer's events
+    %draw events one by one by Gammar distribution (consider incontinuous points) -> t
+    current=[current; [t,zeros(length(t),1)+i]];
+  end
+  closed=[closed;current]
+end
 
 
 %format for output
-t=closed(:,2)==1:n;
-for i=1:n
-  data(i)=closed(1,find(t(:,i)));
-end
+data=labelData2ArrayData(closed,n);
 
 end; %main function
 
 %helper functions:
-function time=drawInterval(lambda,num=1)
+function time=drawIntervalExp(lambda,num=1)
   time=exprnd(lambda,num,1);
+end
+
+function time=drawIntervalGam(shape,rate,num=1)
+  time=gamrnd(shape,1/rate,num,1);%gamrnd takes shape and scale (1/rate) parameters
 end
 
