2ab9d3d03325f82ac3173874c40920e1337438b0
yanxiangtianji <yanxiangtianji@gmail.com>
2015-06-12 03:35:32 -0400

    add a function for drawing ROC curve and calcuating AUROC

--- mRegression/_for_test.m
+++ mRegression/_for_test.m
@@ -27,3 +27,22 @@ function [CMarr]=test_list_AW(fn_list,n_trial,D,A,W)
   end
 end
 
+%roc curve
+function [auroc,roc]=calROC(CM,draw=0)
+  if(iscell(CM)) CM=cell2mat(CM(:));  end;
+  roc=zeros(size(CM,1),2);  %fpRate, tpRate
+  roc(:,1)=CM(:,3)./(CM(:,3)+CM(:,4));  %fpRate
+  roc(:,2)=CM(:,1)./(CM(:,1)+CM(:,2));  %tpRate
+  idx=union(find(isnan(roc(:,1))),find(isnan(roc(:,2))));%remove NaN entries
+  roc(idx,:)=[];
+  roc=[0,0;sortrows(roc);1,1];
+  if(draw)
+    plot(roc(:,1),roc(:,2),[0,1],[0,1]);xlabel('FPR');ylabel('TPR');
+  end
+  n=size(roc,1);
+  auroc=0;
+  for i=2:n
+    auroc+=(roc(i,1)-roc(i-1,1))*(roc(i,2)+roc(i-1,2))/2;
+  end
+end
+
