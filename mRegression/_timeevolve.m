fcf6ca08af478ca7fd5d6807f1bca0f5763e2fb8
yanxiangtianji <yanxiangtianji@gmail.com>
2015-06-16 17:58:55 -0400

    add codes for _timeevolve

--- mRegression/_timeevolve.m
+++ mRegression/_timeevolve.m
@@ -1,9 +1,9 @@
-%W for whole trial
+#W for whole trial
 
 [~,W]=goTogether2(fnlist,19,D,fRep,1,1,Ainit,Winit);
 %load '../data/dataTGH.mat'
 
-%Aarr for each small period given W
+#Aarr for each small period given W
 
 nPeriod=9;
 periodLength=10*timeUnit2ms;
@@ -12,15 +12,28 @@ for i=1:m; for j=1:4;
   rData=readRaw(fnlist(i,j));
   for k=1:nPeriod;
     data=pickDataByTime(rData,k*periodLength,(k+1)*periodLength);
-    Aarr(i,j,k)=trainAFixW(data,D,lambdaA,fRep,Ainit,W);
+    t=trainAFixW(data,D,lambdaA,fRep,Ainit,W);
+    for l=1:n;
+      if(length(data(l))==0)
+        t(:,l)=0;
+      end;
+    end;
+    Aarr(i,j,k)=t;
   end;
 end;end;
 
-%backbone of each period
+save('../data/dataPer.mat','D','lambdaA','lambdaW','fRep','Ainit','Winit','nPeriod','W','Aarr','Aa','As')
+
+#backbone of each period
 Aa=cell(4,nPeriod); As=cell(4,nPeriod);
 for i=1:4;for j=1:nPeriod;  [Aa(i,j),As(i,j)]=statMat(Aarr(:,i,j));   end;end;
 
-%plot backbone,distribtion of +/- of each period
+bone=findBoneAvg(Aa,0.5);
 
-
+#plot backbone,distribtion of +/- of each period
+cue_id=1;
+figure;
+for i=1:nPeriod;
+  subplot(3,3,i);imagesc(cell2mat(bone(cue_id,i)));colorbar;colormap(gray(2));
+end;
 
