263d9b0b85f009bb45e1e78eaabf039c3fd1944a
yanxiangtianji <yanxiangtianji@gmail.com>
2015-06-19 02:04:53 -0400

    consider matrix input of discretize function

--- dataAnalysis/discretize.m
+++ dataAnalysis/discretize.m
@@ -3,7 +3,7 @@ function result=discretize(value,binSize=1,offset=0,resLength=0)
 if(iscell(value)) value=cell2mat(value)(:); end
 
 value=ceil((value-offset)./binSize);
-[idx,~,j]=unique(value);
+[idx,~,j]=unique(value(:)); %make idx and j to be column vectors
 if(resLength!=0 && idx(end)>resLength)
   maxIDX=min(find(idx>resLength))-1;
   idx=idx(range);
