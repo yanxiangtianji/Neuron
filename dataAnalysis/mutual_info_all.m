e5e8429e59a215a0f361059a2e5577b42b1b994b
yanxiangtianji <yanxiangtianji@gmail.com>
2015-06-19 02:05:44 -0400

    bug fix of mutual information mat function

--- dataAnalysis/mutual_info_all.m
+++ dataAnalysis/mutual_info_all.m
@@ -1,12 +1,12 @@
 % Mutual information for all columns of Data
-function M = mutual_info_mat(Data)  
+function M = mutual_info_all(Data)
   [rows,cols] = size(Data);
   M = zeros(cols,cols);
   % Loop through each pair of columns in the alignment
   for i = 1:cols-1
     for j = i+1:cols
       % Calculate the mutual information between two specific columns
-      M(i,j) = mutinf(Data(:,i),Data(:,j));
+      M(i,j) = mutual_info(Data(:,i),Data(:,j));
     end
   end
   
