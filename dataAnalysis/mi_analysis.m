f15e0c041cf56c75f13a2956119d9ac708dbd541
yanxiangtianji <yanxiangtianji@gmail.com>
2015-06-20 06:58:35 -0400

    add script for drawing all X-neuron lines

--- dataAnalysis/mi_analysis.m
+++ dataAnalysis/mi_analysis.m
@@ -10,6 +10,7 @@ for i=1:n
   maxTime=max(maxTime,cell2mat(rData(i))(end));
 end
 
+window_size=10.^[1:9]*timeUnit2ms;
 window_size=[10:10:50, 100:50:300, 400:100:1000, 2000:1000:10000]*timeUnit2ms;
 mi=zeros(n,n,length(window_size));
 for i=1:length(window_size);
@@ -33,3 +34,14 @@ title(['MI between ',num2str(i),' and ',num2str(j)]);
 hold on
 for i=1:19;for j=i+1:19;plot(window_size/timeUnit2ms,mi(i,j,:)(:));end;end;
 hold off
+
+idx=find(mi(:,:,1));
+mi2=zeros(length(window_size),length(idx));
+for i=1:length(idx);
+  ii=mod(idx(i)-1,n)+1;
+  jj=ceil(idx(i)/n);
+  mi2(:,i)=mi(ii,jj,:)(:);
+end;
+plot(log10(window_size/timeUnit2ms),mi2)
+
+
