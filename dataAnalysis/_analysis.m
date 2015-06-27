6027cbd38e0b7246f0d70a1cceed77a29633a81b
yanxiangtianji <yanxiangtianji@gmail.com>
2015-06-27 00:22:03 -0400

    change mi_analysis to generalized _analysis (add dot-product metric, pearson correaltion metric).

--- dataAnalysis/_analysis.m
+++ dataAnalysis/_analysis.m
@@ -7,34 +7,44 @@ basicParameters
 rData=readRaw([fn_prefix,'all.txt']);
 rmpath('../mRegression/')
 
-function maxTime=findMaxTime(rDataList)
-  maxTime=0;
-  [l1,l2]=size(rDataList);
-  for i=1:l1;for j=1:l2;
-    if(isempty(t=cell2mat(rDataList(i,j)))==0)
-      maxTime=max(maxTime,t(end));
-    end;
-  end;end;
-end
+%mutual information:
+global disFun=@(data,ws,off,len)discretize(data,ws,off,len,'count');
+global infoFun=@(data1,data2)mutual_info(data1,data2);
+%dot product:
+global disFun=@(data,ws,off,len)discretize(data,ws,off,len,'binary');
+global infoFun=@(data1,data2)dot_product(data1,data2);
+%Pearson correlation:
+global disFun=@(data,ws,off,len)discretize(data,ws,off,len,'count');
+global infoFun=@(data1,data2)corr(data1(:),data2(:));
 
 maxTime=findMaxTime(rData);%=40000000, log10(maxTime/timeUnit2ms)=7+log10(4)
 
 window_size=((1:2:10)'*(10.^(1:6)))(:)*timeUnit2ms; %log-scale
-window_size=[0.1:0.2:2,2:10]*10000*timeUnit2ms;  %around peak 
-mi=zeros(nNeu,nNeu,length(window_size));
-for i=1:length(window_size);
-  tic;
-  ws=window_size(i);
-  vecLength=ceil(maxTime/ws);
-  data=zeros(vecLength,nNeu);
-  for j=1:nNeu;
-    data(:,j)=discretize(cell2mat(rData(j)),ws,0,vecLength);
-  end;
-  mi(:,:,i)=mutual_info_all(data);
-  toc;
-end;
+window_size=round([0.1:0.2:2, 2:10]*10000)*timeUnit2ms;  %around peak 
 
-save('Xneu.mat','window_size','mi');
+function mi=calMI_xn(rData,window_size)
+  nNeu=length(rData);
+  maxTime=findMaxTime(rData);
+  mi=zeros(nNeu,nNeu,length(window_size));
+  global disFun infoFun
+  for wid=1:length(window_size);
+    tic;
+    ws=window_size(wid);
+    vecLength=ceil(maxTime/ws);
+    data=zeros(vecLength,nNeu);
+    for j=1:nNeu;
+      data(:,j)=disFun(cell2mat(rData(j)),ws,0,vecLength);
+    end;
+    for i=1:nNeu;for j=i+1:nNeu;
+      mi(i,j,wid)=infoFun(data(:,i),data(:,j));
+    end;end;
+    toc;
+  end;
+end
+mi=calMI_xn(rData,window_size);
+
+save('Xneu-log.mat','window_size','mi');
+save('Xneu-focus.mat','window_size','mi');
 
 i=6;j=14;
 plot(window_size/timeUnit2ms,mi(i,j,:)(:));xlabel('windows size(ms)');ylabel('MI');
@@ -77,16 +87,18 @@ rmpath('../mRegression/')
 maxTime=findMaxTime(rDataList);%=100000, log10(maxTime/timeUnit2ms)=5
 
 window_size=[((1:2:10)'*(10.^(1:3)))(:);10^4]*timeUnit2ms; %log-scale
-window_size=[0.05,0.1:0.2:2,2:8]*1000*timeUnit2ms;  %around peak 
+window_size=[0.5, 1:2:20, 20]*100*timeUnit2ms;  %around peak 
 
 function mi=calMI_xt_one(rDataList,idx,maxTime,ws)
   nTri=size(rDataList,1);
   vecLength=ceil(maxTime/ws);
   data=zeros(vecLength,nTri);
+  global disFun infoFun
   for i=1:nTri;
-    data(:,i)=discretize(cell2mat(rDataList(i,idx)),ws,0,vecLength);
+    data(:,i)=disFun(cell2mat(rDataList(i,idx)),ws,0,vecLength);
   end;
-  mi=mutual_info_all(data);
+  mi=zeros(nTri);
+  for i=1:nTri;for j=i+1:nTri; mi(i,j)=infoFun(data(:,i),data(:,j)); end;end;
 end
 function mi=calMI_xt(rDataList,window_size)
   nNeu=size(rDataList,2);
@@ -102,10 +114,11 @@ end
 %type: mi=cell(nNeu,length(window_size),nCue); mi(1)=zeros(nTri);
 mi=cell(nNeu,length(window_size),nCue);
 for i=1:nCue;
-  tic;mi(:,16,i)=calMI_xt(rDataList(:,:,i),window_size(16));toc;
+  tic;mi(:,:,i)=calMI_xt(rDataList(:,:,i),window_size);toc;
 end;
 
 save('Xtrial-log.mat','window_size','mi');
+save('Xtrial-focus.mat','window_size','mi');
 
 %figure for mean MI over all trial-pairs of same cue
 function showMI_xt(mi,xPoints,cue_name,nTri)
@@ -162,20 +175,20 @@ function mi=calMI_xc_one(rDataList1,rDataList2,idx,maxTime,ws)
   vecLength=ceil(maxTime/ws);
   l1=length(rDataList1); l2=length(rDataList2);
   data1=zeros(vecLength,length(rDataList1));
+  global disFun infoFun
   for i=1:l1;
-    data1(:,i)=discretize(cell2mat(rDataList1(i,idx)),ws,0,vecLength);    
+    data1(:,i)=disFun(cell2mat(rDataList1(i,idx)),ws,0,vecLength);    
   end;
   mi=0; %mi=zeros(l1,l2);
   for i=1:l2;
     data2=discretize(cell2mat(rDataList2(i,idx)),ws,0,vecLength);    
     for j=1:l1;
-      mi+=mutual_info(data2,data1(:,j));
-      %mi(j,i)=mutual_info(data1(:,j),data2);
+      mi+=infoFun(data2,data1(:,j));
+      %mi(j,i)=infoFun(data2,data1(:,j));
     end
   end;
   mi/=l1*l2;
 end
-
 function mi=calMI_xc(rDataList,window_size)
   [nTri,nNeu,nCue]=size(rDataList);
   maxTime=zeros(nCue,1);
@@ -196,12 +209,14 @@ function mi=calMI_xc(rDataList,window_size)
 end
 
 window_size=[((1:2:10)'*(10.^(1:3)))(:);10^4]*timeUnit2ms; %log-scale
-window_size=[0.05,0.1:0.2:2,2:5]*1000*timeUnit2ms;  %around peak 
+window_size=[0.5, 1:2:20, 20]*100*timeUnit2ms;  %around peak 
 
 %type: mi=cell(nNeu,length(window_size)); mi(1)=zeros(nCue);
 mi=calMI_xc(rDataList,window_size);
-
 save('Xcue-log.mat','window_size','mi');
+
+save('Xcue-focus.mat','window_size','mi');
+
 
 function showMI_xc(mi,xPoints,nid,nCue)
   [nNeu,l]=size(mi);
@@ -214,13 +229,18 @@ function showMI_xc(mi,xPoints,nid,nCue)
   sort(idx);
   data=zeros(length(idx),l);
   for j=1:l;  data(:,j)=cell2mat(mi(nid,j))(idx); end;
-  plot(xPoints,data);grid;set(gca,'xtick',1:4);xlabel('log10(WS)');%ylabel('MI');
-  title(['N',num2str(nid),' X-cue MI']);
-  %legend(ticks)
+  plot(xPoints,data);%legend(ticks)
 end
 
-for nid=1:nNeu;
-  subplot(4,5,i);showMI_xc(mi,log10(window_size/timeUnit2ms),nid,nCue);
+for nid=1:nNeu;%log-scale
+  subplot(4,5,nid);showMI_xc(mi,log10(window_size/timeUnit2ms),nid,nCue);grid;set(gca,'xtick',1:4)
+  title(['N',num2str(nid),' X-cue MI']);xlabel('log10(WS)');%ylabel('MI');
+end;
+
+for nid=1:nNeu;%linear-scale
+  subplot(4,5,nid);showMI_xc(mi,(window_size/timeUnit2ms),nid,nCue);
+  set(gca,'xtick',[5:5:20]*100);set(gca,'xticklabel',{'';'1s';'';'2s'});grid;
+  title(['N',num2str(nid),' X-cue MI']);%xlabel('log10(WS)');ylabel('MI');
 end;
 
 nid=1;
