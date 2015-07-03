a484307b49be2824b55eeed645c8c1dcf9586109
yanxiangtianji <yanxiangtianji@gmail.com>
2015-07-02 23:19:20 -0400

    add script for single spike DP/MI, including information calculation and plotting (experiment+theory)

--- dataAnalysis/_synthetic.m
+++ dataAnalysis/_synthetic.m
@@ -1,8 +1,49 @@
-function dp=fun(n,L,r,window_size,nSample=100,type='DP')
+#################
+#single spike trend
+function info=cal_single(l,e,x,rng,type='DP')
+  info=zeros(length(rng),1);
+  if(strcmp(type,'DP'))
+    for i=1:length(rng);w=rng(i);nBin=ceil(l/w);
+      info(i)=dot_product(discretize(x,w,0,nBin,'binary'),discretize(x+e,w,0,nBin,'binary'));
+    end;
+  else  %MI
+    for i=1:length(rng);w=rng(i);nBin=ceil(l/w);
+      info(i)=mutual_info(discretize(x,w,0,nBin,'count'),discretize(x+e,w,0,nBin,'count'));
+    end;
+ end
+end
+function show_single(rng,info,x,e,type='DP')
+  plot(rng,info,'-*',[x x+e],0,'rh');set(gca,'xtick',0:0.1:1);grid;
+  xValues=[x x+e x+e x];yValues=[0 0 1 1];
+  if(strcmp(type,'DP'))
+    line([0 0.5],[0 1],'linestyle','--','color','r');
+    patch([0 e e],[0 0 e*2],'g');
+    i=1;while(x/i>=e) patch(xValues/i,min(1,yValues*2.*xValues/i),'y'); ++i; end;
+  else  %MI
+    line([0 0.5],[0 1],'linestyle','--','color','r');
+    patch([0 e e],[0 0 e*2],'g');
+    i=1;while(x/i>=e) patch(xValues/i,yValues,'y'); ++i; end;
+  end
+end
+
+rng=[0.01,0.025:0.025:1];
+type='DP';
+type='MI';
+
+l=1;e=0.1;
+x=rand()*(l-e);
+dp=cal_single(l,e,x,rng,type);show_single(rng,dp,x,e,type);
+
+#################
+#multiple spike trend
+function dp=cal_multiple(L,n,r,window_size,nSample=100,type='DP')
   dp=zeros(length(window_size),1);
   for i=1:nSample;
-    x=randi(L,n,1);
-    y=max(0,x+randi(2*r+1,n,1)-r-1);
+    x=randi(L,n,1); %uniform
+    x=cumsum(exprnd(L/n,n,1));x=find(x<=L);%poisson process
+    rnd=unifrnd(-r,r,n,1);%uniform
+%    rnd=normrnd(0,r/3,n,1);%normal
+    y=max(0,x+rnd);
     for wid=1:length(window_size);
       ws=window_size(wid);
       if(strcmp(type,'DP'))
@@ -18,7 +59,7 @@ function dp=fun(n,L,r,window_size,nSample=100,type='DP')
   end;
   dp/=nSample;
 end;
-dp=fun(100,5000,50,window_size,10);plot(log10(window_size),dp);
+dp=cal_multiple(5000,100,50,window_size,10);plot(log10(window_size),dp);
 
 window_size=unique(round(10.^(0:0.2:4)));
 type='DP';
@@ -28,7 +69,7 @@ type='MI';
 radius=[10:10:150];
 dp=zeros(length(window_size),length(radius));
 for i=1:length(radius);
-  dp(:,i)=fun(500,5000,radius(i),window_size,10,type);
+  dp(:,i)=cal_multiple(5000,500,radius(i),window_size,10,type);
 end
 plot(log10(window_size),dp);xlabel('log10(WS)');
 ylabel(type);legend(num2str(radius'));title([type,' on various radius']);
@@ -36,10 +77,11 @@ plot(window_size(1:14),dp(1:14,:));xlabel('WS');
 
 %trend on spike rate
 rate=[.05:.05:.5];
-rate=[.01 .05 .1:.1:.5];
+rate=[0.001*2.^(1:7)]
+L=5000;
 dp2=zeros(length(window_size),length(rate));
 for i=1:length(rate);
-  dp2(:,i)=fun(round(500*rate(i)),5000,50,window_size,10);
+  dp2(:,i)=cal_multiple(L,round(L*rate(i)),50,window_size,10);
 end
 plot(log10(window_size),dp2);xlabel('log10(WS)');
 ylabel(type);legend(num2str(rate'));title([type,' on various rate']);
