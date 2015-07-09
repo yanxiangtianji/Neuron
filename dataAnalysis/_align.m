954e199e08c2acebcbf88d872f9758f0f098adaa
yanxiangtianji <yanxiangtianji@gmail.com>
2015-07-08 23:24:52 -0400

    add script of max-min align algorithm

--- dataAnalysis/_align.m
+++ dataAnalysis/_align.m
@@ -1,10 +1,17 @@
 addpath('../mBasic/')
 nTri=50;
 basicDataParameters
+clear fn_c1 fn_c2 fn_r1 fn_r2
 
-rData=readList(fn_c1);
+rData=readList(fnlist(:,1));
+
 rData=cell(nTri,nNeu,nCue);
 for i=1:nCue; rData(:,:,i)=readList(fnlist(:,i)); end;
+
+####################
+# algorithm 1
+# knn
+####################
 
 function ref=initRef_binMean(rData,maxTime,ws,threshold)
   %require: data=cell(nTri); WindowSize>0
@@ -38,10 +45,35 @@ for nid=1:nNeu;
   ref(nid)=initRef_binMean(rData(:,nid,cid),maxTime,ws,0.3);
 end;
 
-%align
+%align-knn
 center=cell(nNeu,1);error=zeros(nNeu,1);
 for nid=1:nNeu;
   [center(i),error(i)]=knn(rData(:,nid,cid),ref(i),400,1e-4);
 end
+
+####################
+# algorithm 2
+# pairwise iteration of max-min
+####################
+
+function w=pairMMwindow(d1,d2,iter=100,w0=0)
+  %init d1&d2 to row vectors , basic check
+  if(iscell(d1));  d1=cell2mat(d1)(:)';  end;
+  if(isempty(d1)) w=0;  return; end;
+  if(iscell(d2));  d2=cell2mat(d2)(:)';  end;
+  if(isempty(d2)) w=0;  return; end;
+  if(w0==0) w0=2*min(d1(end)/length(d1),d2(end)/length(d2));  end
+  %max-min
+  ref=d2+[-w0,w0]';
+  cmp=d1>ref(1,:)' & d1<ref(2,:)';
+  w=0;
+  for i=1:length(d2)
+    dis=min(abs( d1(find(cmp(i,:)))-d2(i) ));
+    if(isempty(dis)==0);  w=max(w, dis); end;
+  end
+end
+
+w=pairMMwindow(rData(1,11),rData(2,11),100)
+
 
 
