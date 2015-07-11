8fa27e1cb132ae12a928f48554d168e825f3e866
yanxiangtianji <yanxiangtianji@gmail.com>
2015-07-10 23:02:34 -0400

    add script for WindowSize vs. CoverageRate using Max-Min algorithm

--- dataAnalysis/_align.m
+++ dataAnalysis/_align.m
@@ -3,7 +3,7 @@ nTri=50;
 basicDataParameters
 clear fn_c1 fn_c2 fn_r1 fn_r2
 
-rData=readList(fnlist(:,1));
+rData=readList(fnlist(:,1));%for test
 
 rData=cell(nTri,nNeu,nCue);
 for i=1:nCue; rData(:,:,i)=readList(fnlist(:,i)); end;
@@ -56,24 +56,46 @@ end
 # pairwise iteration of max-min
 ####################
 
-function w=pairMMwindow(d1,d2,iter=100,w0=0)
-  %init d1&d2 to row vectors , basic check
-  if(iscell(d1));  d1=cell2mat(d1)(:)';  end;
-  if(isempty(d1)) w=0;  return; end;
-  if(iscell(d2));  d2=cell2mat(d2)(:)';  end;
-  if(isempty(d2)) w=0;  return; end;
-  if(w0==0) w0=2*min(d1(end)/length(d1),d2(end)/length(d2));  end
-  %max-min
+function [w,c]=_pairMMWref(d1,d2,w0)
+  w=0;c=0;
+  if(iscell(d1));  d1=cell2mat(d1);  end;
+  if(isempty(d1)) return; end;
+  if(iscell(d2));  d2=cell2mat(d2);  end;
+  if(isempty(d2)) return; end;
+  if(isrow(d1)==0) d1=reshape(d1,1,numel(d1));  end;
+  if(isrow(d2)==0) d2=reshape(d2,1,numel(d2));  end;
   ref=d2+[-w0,w0]';
   cmp=d1>ref(1,:)' & d1<ref(2,:)';
-  w=0;
   for i=1:length(d2)
-    dis=min(abs( d1(find(cmp(i,:)))-d2(i) ));
-    if(isempty(dis)==0);  w=max(w, dis); end;
+    minDis=min(abs( d1(find(cmp(i,:)))-d2(i) ));
+    if(isempty(dis)==0);  ++c; w=max(w, minDis); end;
+  end
+  c/=length(d1);
+end
+
+function [ws,cr]=pairMMW(d1,d2,compact=0)
+%give minimum Window Size for each Coverage Ratio
+  %init to row vector & basic check
+  ws=cr=[0];
+  if(iscell(d1));  d1=cell2mat(d1);  end;
+  if(isempty(d1)) return; end;
+  if(iscell(d2));  d2=cell2mat(d2);  end;
+  if(isempty(d2)) return; end;
+  if(isrow(d1)==0) d1=reshape(d1,1,numel(d1));  end;
+  if(isrow(d2)==0) d2=reshape(d2,1,numel(d2));  end;
+  ld1=length(d1); ld2=length(d2);
+  %main:
+  cr=(1:ld1)'./ld1;
+  %dis=abs(d1-d2');%size=length(d2),length(d1)
+  ws=sort(min(abs(d1-d2')));
+  if(compact)
+    [ws,idx]=unique(ws);%idx is the last occurence in ws
+    cr=cr(idx);
   end
 end
 
-w=pairMMwindow(rData(1,11),rData(2,11),100)
-
+w=_pairMMWref(rData(1,11),rData(2,11),100)
+[ws,cr]=pairMMW(rData(1,11),rData(2,11));
+plot(ws,cr);
 
 
