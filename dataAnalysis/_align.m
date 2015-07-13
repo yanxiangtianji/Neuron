c111e062184d8647b7d3bc985935068362ef1705
yanxiangtianji <yanxiangtianji@gmail.com>
2015-07-12 21:50:07 -0400

    1, use absolute path 2, move private functions to a sub folder. 3, add script for cross trial MMW

--- dataAnalysis/_align.m
+++ dataAnalysis/_align.m
@@ -1,9 +1,10 @@
-addpath('../mBasic/')
+addpath([pwd,'/../mBasic/'])
 nTri=50;
 basicDataParameters
 clear fn_c1 fn_c2 fn_r1 fn_r2
+addpath([pwd,'/align/'])
 
-rData=readList(fnlist(:,1));%for test
+%rData=readList(fnlist(:,1));%for test
 
 rData=cell(nTri,nNeu,nCue);
 for i=1:nCue; rData(:,:,i)=readList(fnlist(:,i)); end;
@@ -13,30 +14,10 @@ for i=1:nCue; rData(:,:,i)=readList(fnlist(:,i)); end;
 # knn
 ####################
 
-function ref=initRef_binMean(rData,maxTime,ws,threshold)
-  %require: data=cell(nTri); WindowSize>0
-  nTri=length(rData(:));
-  len=ceil(maxTime/ws);
-  disData=zeros(len,nTri);
-  for i=1:nTri;
-    disData(:,i)=discretize(rData(i),ws,0,len,'binary');
-  end;
-  workBin=find(sum(disData,2)>=threshold*nTri);
-  ref=zeros(length(workBin),1);
-  for i=1:nTri;
-    d=cell2mat(rData(i));
-    for j=1:length(workBin);
-      rng=[workBin(j)-1,workBin(j)]*ws;
-      t=mean(d(find(d>=rng(1) & d<rng(2))));
-      if(isempty(t)) t=0; end;
-      ref(j)+=t;
-    end;
-  end;
-  ref/=nTri;
-end
-
 maxTime=findMaxTime(rData);
 ws=200*timeUnit2ms;
+
+%function ref=initRef_binMean(rData,maxTime,ws,threshold)
 
 %ref
 ref=cell(nNeu,1);
@@ -73,29 +54,24 @@ function [w,c]=_pairMMWref(d1,d2,w0)
   c/=length(d1);
 end
 
-function [ws,cr]=pairMMW(d1,d2,compact=0)
-%give minimum Window Size for each Coverage Ratio
-  %init to row vector & basic check
-  ws=cr=[0];
-  if(iscell(d1));  d1=cell2mat(d1);  end;
-  if(isempty(d1)) return; end;
-  if(iscell(d2));  d2=cell2mat(d2);  end;
-  if(isempty(d2)) return; end;
-  if(isrow(d1)==0) d1=reshape(d1,1,numel(d1));  end;
-  if(isrow(d2)==0) d2=reshape(d2,1,numel(d2));  end;
-  ld1=length(d1); ld2=length(d2);
-  %main:
-  cr=(1:ld1)'./ld1;
-  %dis=abs(d1-d2');%size=length(d2),length(d1)
-  ws=sort(min(abs(d1-d2')));
-  if(compact)
-    [ws,idx]=unique(ws);%idx is the last occurence in ws
-    cr=cr(idx);
-  end
-end
+%function [ws,cr]=compactWC2quantile(ws,cr,quan)  %ws and cr are column vectors
+%function [ws,cr]=pairMMW(d1,d2,compact=false)  %ws and cr are column vectors
 
 w=_pairMMWref(rData(1,11),rData(2,11),100)
 [ws,cr]=pairMMW(rData(1,11),rData(2,11));
 plot(ws,cr);
 
+
+ws=cell(nTri,nTri,nNeu,nCue);
+cr=cell(nTri,nTri,nNeu,nCue);
+for cid=1:nCue;
+  for nid=1:nNeu;
+    tic;
+    for tid1=1:nTri;  for tid2=tid1+1:nTri
+      [ws(tid1,tid2,nid,cid),cr(tid1,tid2,nid,cid)]=pairMMW(rData(tid1,nid,cid),rData(tid2,nid,cid));
+    end;end
+    toc;
+  end
+end
+save('./align/base.mat','ws','cr')
 
