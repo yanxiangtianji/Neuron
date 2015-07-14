fe65cbb70491ac032594b9b6144e32f4914b1793
yanxiangtianji <yanxiangtianji@gmail.com>
2015-07-14 03:22:10 -0400

    bug fix

--- dataAnalysis/align/compactWC2quantile.m
+++ dataAnalysis/align/compactWC2quantile.m
@@ -4,16 +4,20 @@ function [ws,cr]=compactWC2quantile(ws,cr,quan,mode='near')
   %'linear':  linear interpolatie cr and ws to exactly meet the input quan
   if(iscell(ws))  ws=cell2mat(ws);  end;
   if(iscell(cr))  cr=cell2mat(cr);  end;
-  if(iscolumn(cr)==0) cr=reshape(cr,numel(cr),1); end;
-  quan=unique(quan(:)');
+  quan=unique(quan(:)');  %row vector
   if(strcmp(mode,'near'))
-    [~,idx]=min(abs(quan-cr));%bsxfun(@minus,quan,cr)
+    [~,idx]=min(abs(quan-cr(:)));%bsxfun(@minus,quan,cr)
     ws=ws(idx);cr=cr(idx);
   elseif(strcmp(mode,'linear'))
-    [~,idx]=min(abs(quan-cr));
-    t=cr(idx)'-quan;
+    [~,idx]=min(abs(quan-cr(:)),[],1);
+    idx=idx';%ensure ws(idx) is a column vector, when ws is a scalar
+    t=(quan-cr(idx)(:)')';%column vector
     idx2=idx+(t>0)-(t<0);
-    idx2=min(max(idx2,1),length(cr));
-    ws=(ws(idx)+ws(idx2))/2;cr=cr(idx);
+    idx2=min(max(idx2,1),length(ws));
+    if(iscolumn(ws)==0)  ws=ws(:);  end;
+    if(iscolumn(cr)==0)  cr=cr(:);  end;
+    x=(ws(idx2)-ws(idx))./(cr(idx2)-cr(idx)).*t;
+    ws=ws(idx);ws(find(isnan(x)==0))+=x(find(isnan(x)==0));
+    cr=cr(idx);
   end
 end
