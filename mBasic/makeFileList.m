621df64bf694ddd5ace67372422a736a3fd9c373
yanxiangtianji <yanxiangtianji@gmail.com>
2015-07-16 15:41:07 -0400

    bug fix (do not display inner variable)

--- mBasic/makeFileList.m
+++ mBasic/makeFileList.m
@@ -1,5 +1,5 @@
 function fnlist=makeFileList(nTri,varargin)
-  nCue=length(varargin)
+  nCue=length(varargin);
   fnlist=cell(nTri,nCue);
   for i=1:nCue;
     fnlist(:,i)=cell2mat(varargin(i))(1:nTri);
