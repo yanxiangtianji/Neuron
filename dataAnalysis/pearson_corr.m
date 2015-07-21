a35b7745c6f72514954eb9d2b652597ec68b3e0e
yanxiangtianji <yanxiangtianji@gmail.com>
2015-07-21 16:46:59 -0400

    add consideration of non-zero identical case for pearson correlation

--- dataAnalysis/pearson_corr.m
+++ dataAnalysis/pearson_corr.m
@@ -1,7 +1,9 @@
 function pc=pearson_corr(X,Y)
-  if(!any(X) || !any(Y))
+  if(length(X)!=length(Y)) error('X and Y must have the same number of elements'); end
+  s1=std(X);s2=std(Y);
+  if(isempty(X) || isempty(Y) || s1==0 || s2==0)
     pc=0;
   else
-    pc=corr(X,Y);
+    pc=cov(X,Y)/s1/s2;
   end
 end
