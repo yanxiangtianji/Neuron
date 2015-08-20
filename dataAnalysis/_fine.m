527d21e07c07999bd14c6c687d246db4626bfae1
yanxiangtianji <yanxiangtianji@gmail.com>
2015-08-19 20:06:25 -0400

    1, use nCue. 2, use new smooth function. 3, more functions display result

--- dataAnalysis/_fine.m
+++ dataAnalysis/_fine.m
@@ -1,6 +1,7 @@
 #prepare 
 addpath([pwd,'/../mBasic/'])
-nTri=50;nCue=2;
+nTri=50;
+%nCue=2;
 basicDataParameters
 clear fn_c1 fn_c2 fn_r1 fn_r2
 
@@ -8,8 +9,8 @@ data=readRaw([fn_prefix,'all.txt']);
 beh=readCue([fn_prefix,'beh.txt']);
 cue1=beh(find(beh(:,1)==1),:);
 cue2=beh(find(beh(:,1)==2),:);
-cue3=cue1; cue3(:,[2,3])=bsxfun(@plus,cue3(:,[2,3]),cue3(:,4)/2-triLength/2); cue3=cue3(find(cue3(:,4)<=triLength),:);
-cue4=cue2; cue4(:,[2,3])=bsxfun(@plus,cue4(:,[2,3]),cue4(:,4)/2-triLength/2); cue4=cue4(find(cue4(:,4)<=triLength),:);
+cue3=cue1; cue3(:,[2,3])=bsxfun(@plus,cue3(:,[2,3]),cue3(:,4)/2-triLength/2); cue3=cue3(find(cue3(:,4)>=triLength),:);
+cue4=cue2; cue4(:,[2,3])=bsxfun(@plus,cue4(:,[2,3]),cue4(:,4)/2-triLength/2); cue4=cue4(find(cue4(:,4)>=triLength),:);
 cue=[cue1(1:nTri,2),cue2(1:nTri,2),cue3(1:nTri,2),cue4(1:nTri,2)];
 
 binSize=50*timeUnit2ms;
@@ -31,17 +32,18 @@ cueBinEnd=ceil((cue+offsetEnd)/binSize);
 xAxisTicks=(offsetBeg:binSize:offsetEnd)/timeUnit2ms/1000;
 if(length(xAxisTicks)>ceil(maxTime/binSize))xAxisTicks=xAxisTicks(1:ceil(maxTime/binSize));end
 
-dt=cell(nTri,nNeu,2);
-for cid=1:2;for tid=1:nTri;
+dt=cell(nTri,nNeu,nCue);
+for cid=1:nCue;for tid=1:nTri;
   dt(tid,:,cid)=pickRawFromRng(data,cue(tid,cid)+offsetBeg,cue(tid,cid)+offsetEnd);
 end;end;
 
-rt=zeros(ceil(maxTime/binSize),nTri,nNeu,2);
-for cid=1:2; for nid=1:nNeu; for tid=1:nTri;
+rt=zeros(ceil(maxTime/binSize),nTri,nNeu,nCue);
+for cid=1:nCue; for nid=1:nNeu; for tid=1:nTri;
   rt(:,tid,nid,cid)=discretize(dt(tid,nid,cid),binSize,cue(tid,cid)+offsetBeg,ceil(maxTime/binSize),'count');
 end;end;end;
 rtz=zscore(rt);
 
+return;
 ####################
 # z-score (follow David's work)
 ####################
@@ -71,29 +73,46 @@ imagesc(ratesz(cueBinBeg(tid,cid):cueBinEnd(tid,cid),:)');colorbar;ylabel('neuro
 #data is cue-trial period:
 maxTime=offsetEnd-offsetBeg;
 vecLength=ceil(maxTime/binSize);
-rts=zeros(vecLength,nTri,nNeu,2);
-for cid=1:2;
-  tic;for i=1:nid;for tid=1:nTri;
+rts=zeros(vecLength,nTri,nNeu,nCue);
+for cid=1:nCue;
+  tic;for nid=1:nNeu;for tid=1:nTri;
     #rts(:,tid,nid,cid)=regdatasmooth(1:vecLength,rt(:,tid,nid,cid));
-    rts(:,tid,nid,cid)=rgdtsmcore((1:vecLength)',rt(:,tid,nid,cid),2,2);
+    #rts(:,tid,nid,cid)=rgdtsmcore((1:vecLength)',rt(:,tid,nid,cid),2,2);
+    rts(:,tid,nid,cid)=max(0,smooth(rt(:,tid,nid,cid)',9,'sgolay')');
   end;end;toc;
 end;
 save('rt_s.mat','rts');
 rtsz=zscore(rts);
-%figure: rate of one trial of one neuron
+
+%figure: rate of ONE trial of ONE neuron
+function show_rt_one(rt,tid,nid,xAxisTicks,cue_name)
+  [vecLength,~,~,nCue]=size(rt);
+  t=reshape(rt(:,tid,nid,:),vecLength,nCue);
+  plot(xAxisTicks,t);legend(cue_name);legend('boxoff');
+  title(['neuron-',num2str(nid),',trial-',num2str(tid)])
+end
+
 tid=1;nid=10;cid=1;
-plot(xAxisTicks,rtsz(:,tid,nid,1),xAxisTicks,rtsz(:,tid,nid,2));
-errorbar(xAxisTicks,mean(rt(:,:,nid,1),2),std(rt(:,:,nid,1),0,2));
-%figure: rate of all neurons on one trial
+show_rt_one(rtsz,tid,nid,xAxisTicks,cue_name);
+%title(['rtsz: ',get(get(gca,'title'),'string')]);
+
+function show_rt_eb(rt,nid,cid,xAxisTicks)
+  t=rt(:,:,nid,cid)';
+  errorbar(xAxisTicks,mean(t),std(t));xlim([xAxisTicks(1),xAxisTicks(end)])
+end;
+show_rt_eb(rt,nid,cid,xAxisTicks)
+
+%figure: rate of ALL trials on ONE neuron
 function show_rt_xt(rt,xPoints)
   [nTri,vecLength]=size(rt);
   imagesc(rt);colorbar;ylabel('trial');xlabel('time');
-  %title(['cue: ',num2str(cid),',neuron: ',num2str(nid)])
+  title(['cue-',num2str(cid),',neuron-',num2str(nid)])
   l=length(xPoints);
   set(gca,'xtick',(0:l-1)*vecLength/max(1,l-1));set(gca,'xticklabel',xPoints);
 end
-function show_rt4(nid,cid,rt,rtz,rts,rtsz,xPoints)
+function show_rt_m(nid,cid,rt,rtz,rts,rtsz,xPoints)
   subplot(2,2,1);show_rt_xt(rt(:,:,nid,cid)',  xPoints);title('rt')
+  %title(['rt: ',get(get(gca,'title'),'string')]);
   subplot(2,2,2);show_rt_xt(rtz(:,:,nid,cid)', xPoints);title('rtz')
   subplot(2,2,3);show_rt_xt(rts(:,:,nid,cid)', xPoints);title('rts')
   subplot(2,2,4);show_rt_xt(rtsz(:,:,nid,cid)',xPoints);title('rtsz')
@@ -101,8 +120,9 @@ end
 
 xPoints=(offsetBeg:maxTime/6:offsetEnd)/timeUnit2ms/1000;
 nid=10;cid=1;
-show_rt_xt(rt(:,:,nid,cid)',xPoints);title(['cue: ',num2str(cid),',neuron: ',num2str(nid)])
-show_rt4(nid,cid,rt,rtz,rts,rtsz,xPoints)
+show_rt_m(nid,cid,rt,rtz,rts,rtsz,xPoints)
+
+%show_rt_xt(rt(:,:,nid,cid)',xPoints);title(['cue: ',num2str(cid),',neuron: ',num2str(nid)])
 
 ####################
 # dot-product
