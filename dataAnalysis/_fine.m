38850493e01ebfb6f6b739c7598c83ad0944b8cb
yanxiangtianji <yanxiangtianji@gmail.com>
2015-08-21 17:41:19 -0400

    seperate dedicate functions. fix bugs.

--- dataAnalysis/_fine.m
+++ dataAnalysis/_fine.m
@@ -1,5 +1,6 @@
 #prepare 
 addpath([pwd,'/../mBasic/'])
+addpath([pwd,'/fine/'])
 nTri=50;
 %nCue=2;
 basicDataParameters
@@ -12,6 +13,7 @@ cue2=beh(find(beh(:,1)==2),:);
 cue3=cue1; cue3(:,[2,3])=bsxfun(@plus,cue3(:,[2,3]),cue3(:,4)/2-triLength/2); cue3=cue3(find(cue3(:,4)>=triLength),:);
 cue4=cue2; cue4(:,[2,3])=bsxfun(@plus,cue4(:,[2,3]),cue4(:,4)/2-triLength/2); cue4=cue4(find(cue4(:,4)>=triLength),:);
 cue=[cue1(1:nTri,2),cue2(1:nTri,2),cue3(1:nTri,2),cue4(1:nTri,2)];
+%clear cue1 cue2 cue3 cue4
 
 binSize=50*timeUnit2ms;
 
@@ -28,7 +30,7 @@ offsetBeg=-1000*timeUnit2ms;
 offsetEnd=2000*timeUnit2ms;
 maxTime=offsetEnd-offsetBeg;
 cueBinBeg=ceil((cue+offsetBeg)/binSize);
-cueBinEnd=ceil((cue+offsetEnd)/binSize);
+cueBinEnd=ceil((cue+offsetEnd)/binSize)-1;
 xAxisTicks=(offsetBeg:binSize:offsetEnd)/timeUnit2ms/1000;
 if(length(xAxisTicks)>ceil(maxTime/binSize))xAxisTicks=xAxisTicks(1:ceil(maxTime/binSize));end
 
@@ -42,6 +44,8 @@ for cid=1:nCue; for nid=1:nNeu; for tid=1:nTri;
   rt(:,tid,nid,cid)=discretize(dt(tid,nid,cid),binSize,cue(tid,cid)+offsetBeg,ceil(maxTime/binSize),'count');
 end;end;end;
 rtz=zscore(rt);
+
+%function rt=cutRateDate2Trial(rate,cueBinBeg,cueBinEnd,tids,nids,cids)
 
 return;
 ####################
@@ -63,14 +67,9 @@ end;
 warning on;
 save('rate_s.mat','rates');
 ratesz=zscore(rates);
-%figure: rate of one trial of one neuron
-tid=1;nid=9;cid=1;
-plot(ratesz(cueBinBeg(tid,cid):cueBinEnd(tid,cid),nid));
-%figure: rate of all neurons on one trial
-imagesc(ratesz(cueBinBeg(tid,cid):cueBinEnd(tid,cid),:)');colorbar;ylabel('neuron id');
 
 ####
-#data is cue-trial period:
+#data is cue-trial cut period:
 maxTime=offsetEnd-offsetBeg;
 vecLength=ceil(maxTime/binSize);
 rts=zeros(vecLength,nTri,nNeu,nCue);
@@ -84,17 +83,29 @@ end;
 save('rt_s.mat','rts');
 rtsz=zscore(rts);
 
-%figure: rate of ONE trial of ONE neuron
-function show_rt_one(rt,tid,nid,xAxisTicks,cue_name)
-  [vecLength,~,~,nCue]=size(rt);
-  t=reshape(rt(:,tid,nid,:),vecLength,nCue);
-  plot(xAxisTicks,t);legend(cue_name);legend('boxoff');
-  title(['neuron-',num2str(nid),',trial-',num2str(tid)])
-end
+####
+#Figures:
+vecLength=ceil((offsetEnd-offsetBeg)/binSize);
 
+%figure: rate of ONE trial of ONE neuron ONE cue
 tid=1;nid=10;cid=1;
-show_rt_one(rtsz,tid,nid,xAxisTicks,cue_name);
-%title(['rtsz: ',get(get(gca,'title'),'string')]);
+plot(xAxisTicks,cutRateDate2Trial(rate,cueBinBeg,cueBinEnd,tid,nid,cid));
+plot(xAxisTicks,rtsz(:,tid,nid,cid));
+title([cue_name(cid),',neuron-',num2str(nid),',trial-',num2str(tid)])
+
+%figure: rate of ONE trial of ONE neuron ALL cue
+function show_rt_one_m(data,tid,nid,xAxisTicks,cue_name,titlePrefix='')
+  %data is a 2D matrix
+  clf;hold all; nCue=size(data,2);
+  for cid=1:nCue;
+    plot(xAxisTicks,data(:,cid));
+  end;
+  title([titlePrefix,',neuron-',num2str(nid),',trial-',num2str(tid)])
+  hold off;legend(cue_name);legend('boxoff')
+end
+show_rt_one_m(reshape(rtsz(:,tid,nid,1:nCue),vecLength,nCue),tid,nid,xAxisTicks,cue_name,'rtsz: ');
+show_rt_one_m(reshape(cutRateDate2Trial(rates,cueBinBeg,cueBinEnd,tid,nid,1:nCue),vecLength,nCue),
+  tid,nid,xAxisTicks,cue_name,'rts: ');
 
 function show_rt_eb(rt,nid,cid,xAxisTicks)
   t=rt(:,:,nid,cid)';
@@ -102,25 +113,21 @@ function show_rt_eb(rt,nid,cid,xAxisTicks)
 end;
 show_rt_eb(rt,nid,cid,xAxisTicks)
 
+%figure: rate of ONE trial of ALL neurons
+imagesc(ratesz(cueBinBeg(tid,cid):cueBinEnd(tid,cid),:)');colorbar;ylabel('neuron id');
+
+
 %figure: rate of ALL trials on ONE neuron
-function show_rt_xt(rt,xPoints)
-  [nTri,vecLength]=size(rt);
-  imagesc(rt);colorbar;ylabel('trial');xlabel('time');
-  title(['cue-',num2str(cid),',neuron-',num2str(nid)])
-  l=length(xPoints);
-  set(gca,'xtick',(0:l-1)*vecLength/max(1,l-1));set(gca,'xticklabel',xPoints);
-end
-function show_rt_m(nid,cid,rt,rtz,rts,rtsz,xPoints)
-  subplot(2,2,1);show_rt_xt(rt(:,:,nid,cid)',  xPoints);title('rt')
-  %title(['rt: ',get(get(gca,'title'),'string')]);
-  subplot(2,2,2);show_rt_xt(rtz(:,:,nid,cid)', xPoints);title('rtz')
-  subplot(2,2,3);show_rt_xt(rts(:,:,nid,cid)', xPoints);title('rts')
-  subplot(2,2,4);show_rt_xt(rtsz(:,:,nid,cid)',xPoints);title('rtsz')
-end
+
+%function _show_rt_xt_kernel(rt,xPoints)
+%function show_rt_xt_w(rate,nid,cid,xPoints,cueBinBeg,cueBinEnd,titlePrefix='')
+%function show_rt_w_m(nid,cid,rate,ratez,rates,ratesz,xPoints,cueBinBeg,cueBinEnd)
+%function show_rt_xt_c(rt,nid,cid,xPoints,titlePrefix='')
+%function show_rt_c_m(nid,cid,rt,rtz,rts,rtsz,xPoints)
 
 xPoints=(offsetBeg:maxTime/6:offsetEnd)/timeUnit2ms/1000;
 nid=10;cid=1;
-show_rt_m(nid,cid,rt,rtz,rts,rtsz,xPoints)
+show_rt_m_c(nid,cid,rt,rtz,rts,rtsz,xPoints)
 
 %show_rt_xt(rt(:,:,nid,cid)',xPoints);title(['cue: ',num2str(cid),',neuron: ',num2str(nid)])
 
@@ -128,102 +135,116 @@ show_rt_m(nid,cid,rt,rtz,rts,rtsz,xPoints)
 # dot-product
 ####################
 
+%mutual information:
+global disFun infoFun type
+disFun=@(data,ws,off,len)discretize(data,ws,off,len,'count');
+infoFun=@(data1,data2)mutual_info(data1,data2);
+type='MI';
+%dot product:
+global disFun infoFun type
+disFun=@(data,ws,off,len)discretize(data,ws,off,len,'binary');
+infoFun=@(data1,data2)dot_product(data1,data2);
+type='DP';
+%Pearson correlation:
+global disFun infoFun type
+disFun=@(data,ws,off,len)discretize(data,ws,off,len,'count');
+infoFun=@(data1,data2)pearson_corr(data1(:),data2(:));
+type='PC';
+
 #cross TRIAL:
 
 %whole
-function dp=cal_xt_whole(rate,trialBinBeg,trialBinEnd)
+function info=cal_xt_w(rate,trialBinBeg,trialBinEnd)
   [vecLength,nNeu]=size(rate);
   nTri=length(trialBinBeg);
-  dp=zeros(nTri,nTri,nNeu);
+  info=zeros(nTri,nTri,nNeu);
+  global infoFun
   for nid=1:nNeu; for tid1=1:nTri;
     d1=rate(trialBinBeg(tid1):trialBinEnd(tid1),nid);
     for tid2=tid1+1:nTri;
       d2=rate(trialBinBeg(tid2):trialBinEnd(tid2),nid);
-      dp(tid1,tid2,nid)=dot_product(d1,d2);
+      info(tid1,tid2,nid)=infoFun(d1,d2);
     end;
   end;end;
 end
-dp=zeros(nTri,nTri,nNeu,nCue);
+info_xt=zeros(nTri,nTri,nNeu,nCue);
 for cid=1:nCue;
-  dp(:,:,:,cid)=cal_xt(ratesz,cueBinBeg(:,cid),cueBinEnd(:,cid));
+  tic;info_xt(:,:,:,cid)=cal_xt_w(ratesz,cueBinBeg(:,cid),cueBinEnd(:,cid));toc;
 end
 
 %cue-trial cut
-function dp=cal_xt_cut(rt)
-  %calculate do for only one cue
+function info=cal_xt_c(rt)
+  %calculation goes on only one cue
   [vecLength,nTri,nNeu]=size(rt);
-  dp=zeros(nTri,nTri,nNeu);
+  info=zeros(nTri,nTri,nNeu);
+  global infoFun
   for nid=1:nNeu;
     for tid1=1:nTri; for tid2=tid1+1:nTri;
-      dp(tid1,tid2,nid)=dot_product(rt(:,tid1,nid),rt(:,tid2,nid));
+      info(tid1,tid2,nid)=infoFun(rt(:,tid1,nid),rt(:,tid2,nid));
     end;end;
   end;
 end
-dp=zeros(nTri,nTri,nNeu,nCue);
+info_xt=zeros(nTri,nTri,nNeu,nCue);
 for cid=1:nCue;
-  dp(:,:,:,cid)=cal_xt_cut(rtsz(:,:,:,cid));
+  tic;info_xt(:,:,:,cid)=cal_xt_c(rtsz(:,:,:,cid));toc;
 end
+save('info_xt.mat','info_xt');
 
 %show
-function show_xt_each(dp)
-  [nTri,~,nNeu]=size(dp);
-  vld_idx=find(triu(ones(nTri),1));
-  temp=zeros(nTri*(nTri-1)/2,nNeu);
-  for nid=1:nNeu;  temp(:,nid)=dp(:,:,nid)(vld_idx);  end;
-  imagesc(temp');colorbar;
-end
+%function [mr,mc]=show_xt(info,more=0)
 
-show_xt_each(dp1);xlabel('trial-pair');ylabel('neuron');
-
+cid=1
+show_xt(info_xt(:,:,:,cid));title([cue_name(cid),': x-trial corr.'])
+show_xt(info_xt(:,:,:,cid),1);title([cue_name(cid),': x-trial corr.'])
 nid=10;
-imagesc(dp1(:,:,nid));colorbar;
+imagesc(info_xt(:,:,nid,cid));colorbar;
 
 #cross NEURON:
 %whole
-function dp=cal_xn(rate,trialBinBeg,trialBinEnd)
+function info=cal_xn_w(rate,trialBinBeg,trialBinEnd)
   [vecLength,nNeu]=size(rate);
   nTri=length(trialBinBeg);
-  dp=zeros(nNeu,nNeu,nTri);
+  info=zeros(nNeu,nNeu,nTri);
+  global infoFun
   for tid=1:nTri; for nid1=1:nNeu;
     d1=rate(trialBinBeg(tid):trialBinEnd(tid),nid1);
     for nid2=1:nNeu;
       d2=rate(trialBinBeg(tid):trialBinEnd(tid),nid2);
-      dp(nid1,nid2,tid)=dot_product(d1,d2);
+      info(nid1,nid2,tid)=infoFun(d1,d2);
     end;
   end; end
 end
-dp=zeros(nTri,nTri,nNeu,nCue);
+info_xn=zeros(nNeu,nNeu,nTri,nCue);
 for cid=1:nCue;
-  dp(:,:,:,cid)=cal_xn(rtsz(:,:,:,cid));
+  tic;info_xn(:,:,:,cid)=cal_xn_w(ratesz,cueBinBeg(:,cid),cueBinEnd(:,cid));toc;
 end
 
 %cue-trial cut
-function dp=cal_xn_cut(rt)
+function info=cal_xn_c(rt)
   [vecLength,nTri,nNeu]=size(rt);
-  dp=zeros(nNeu,nNeu,nTri);
+  info=zeros(nNeu,nNeu,nTri);
+  global infoFun
   for tid=1:nTri;
     for nid1=1:nNeu; for nid2=nid1+1:nNeu;
-      dp(nid1,nid2,tid)=dot_product(rt(:,tid,nid1),rt(:,tid,nid2));
+      info(nid1,nid2,tid)=infoFun(rt(:,tid,nid1),rt(:,tid,nid2));
     end;end;
   end
 end
-dp=zeros(nTri,nTri,nNeu,nCue);
+info_xn=zeros(nNeu,nNeu,nTri,nCue);
 for cid=1:nCue;
-  dp(:,:,:,cid)=cal_xn(rtsz(:,:,:,cid));
+  tic;info_xn(:,:,:,cid)=cal_xn_c(rtsz(:,:,:,cid));toc;
 end
+save('info_xn.mat','info_xn')
 
 %show
-function show_xn(dp)
-  [nNeu,~,nTri]=size(dp);
-  vld_idx=find(triu(ones(nNeu),1));
-  temp=zeros(nTri*(nTri-1)/2,nNeu);
-  for tid=1:nTri;  temp(:,tid)=dp(:,:,tid)(vld_idx);  end;
-  imagesc(temp');colorbar;
-end
+%function [mr,mc]=show_xn(info,more=0)
 
-show_xn(dp1);xlabel('neuron-pair');ylabel('trial');
-
+cid=1
+show_xn(info_xn(:,:,:,cid));title([cue_name(cid),': x-neuron corr.'])
+show_xn(info_xn(:,:,:,cid),1);title([cue_name(cid),': x-neuron corr.'])
 tid=1;
-imagesc(dp1(:,:,tid));colorbar;
+imagesc(info_xn(:,:,tid,cid));colorbar;
+
+
 
 
