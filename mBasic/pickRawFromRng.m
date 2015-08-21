0fa844e5e6f5812727c60c1569bda15b19875334
yanxiangtianji <yanxiangtianji@gmail.com>
2015-08-21 09:53:17 -0400

    integrity improvement on rare continuous case

--- mBasic/pickRawFromRng.m
+++ mBasic/pickRawFromRng.m
@@ -2,6 +2,6 @@ function res=pickRawFromRng(data,start_t,end_t)
   res=cell(size(data));
   for i=1:numel(data)
     t=cell2mat(data(i));
-    res(i)=t(find(t>start_t & t<end_t));
+    res(i)=t(find(t>start_t & t<=end_t));
   end
 end
