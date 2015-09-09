37ccadc5fa58c76ada772440218c1f6df0fce8a8
yanxiangtianji <yanxiangtianji@gmail.com>
2015-09-08 21:42:29 -0400

    1, add event transform functions. 2, change the last trial's time-to-next field from constant 0 to time-to-end

--- dataInit/change_format.py
+++ dataInit/change_format.py
@@ -67,7 +67,7 @@ def text2lines_spk(data,sep_item=',',sep_line='\n'):
                 res[i].append(items[i]);
     return res;
 
-def text2lines_beh(data,sep_item=',',sep_line='\n'):
+def text2lines_beh(data,sep_item=',',sep_line='\n',max_time=0):
     #input:  start_time, cue_id/event_id(3 is end of cue)
     #output: cue_id, start_time, end_time, interval_to_next_time
     res=[];
@@ -84,7 +84,7 @@ def text2lines_beh(data,sep_item=',',sep_line='\n'):
         last_state=state;
     for i in range(1,len(res)):
         res[i-1].append(res[i][1]-res[i-1][2])
-    res[-1].append(0)
+    res[-1].append(max(0,max_time-res[-1][2]))
     return res;
 
 def write_lines_spk(filename,data):
@@ -120,13 +120,16 @@ def csv2lineTxt_spk(filename,amp=1,sep_line='\n'):
     data=text2lines_spk(f.read(),',',sep_line);
     f.close();
     data=transAmp(data,amp,0);
+    m=0;
+    for l in data:  m=max(m,max(l))
     fn2=re.sub('\.csv$','.txt',filename);
     if filename==fn2: fn2=filename+'.txt';
     write_lines_spk(fn2,data);
+    return m/amp
 
-def csv2lineTxt_beh(filename,amp=1,sep_line='\n'):
+def csv2lineTxt_beh(filename,amp=1,sep_line='\n',max_time=0):
     f=open(filename);
-    data=text2lines_beh(f.read(),',',sep_line);
+    data=text2lines_beh(f.read(),',',sep_line,max_time);
     f.close();
     data=transAmp(data,amp,1);
     fn2=re.sub('\.csv$','.txt',filename);
@@ -135,42 +138,66 @@ def csv2lineTxt_beh(filename,amp=1,sep_line='\n'):
 
 def enumFileExt(folder,ext):
     l=[];
-    regstr=ext.replace('.','\.')+'$';
+    regstr=ext.replace('.','\\.')+'$';
     for name in os.listdir(folder):
         if os.path.isdir(folder+'/'+name): continue;
         elif re.search(regstr,folder+'/'+name)==None: continue;
         l.append(name)
     return l;
 
-def transAll2txt(folder,n_amp=5):
+def transRec2txt(folder,n_amp=4):
     fn_list=enumFileExt(folder,'.csv')
     fn_spk=[];fn_beh=[];
     amp=10**n_amp;
+    d={}
     for name in fn_list:
-        if name.find('-spike')!=-1:
-#            csv2lineTxt_spk(folder+'/'+name,amp,'\n')
+        p=name.find('-spike');
+        if p!=-1:
+            m=csv2lineTxt_spk(folder+'/'+name,amp,'\n')
+            d[name[0:p]]=m;
             fn_spk.append(name);
-        elif name.find('-beh')!=-1:
-            csv2lineTxt_beh(folder+'/'+name,amp,'\n')
+    d
+    for name in fn_list:
+        p=name.find('-beh');
+        if p!=-1:
+            m=d.get(name[0:p],0);
+            csv2lineTxt_beh(folder+'/'+name,amp,'\n',m)
             fn_beh.append(name);
-        else:
-            print folder+'/'+name, 'not support'
     return (fn_spk,fn_beh)
 
-def merge_spikes(folder,fn_spk,fn_out):
-    fout=open(folder+'/'+fn_out,'w')
-    for fn in fn_spk:
-        f=open(folder+'/'+fn_spk);
-        fout.write(f.read());
-        f.close();
-    fout.close();
+#event:
+
+def event2lineTxt(filenameIn,filenameOUT,amp,sep_item='\t',sep_line='\n'):
+    f=open(filenameIn);
+    data=f.read();
+    f.close()
+    res=[]
+    for line in data.split(sep_line):
+        if len(line)==0:    continue;
+        items=line.split(sep_item);
+        res.append([int(amp*float(items[0])),int(items[1])])
+    write_lines_beh(filenameOUT,res);
+    
+
+def transEvent(folder,n_amp=4):
+    fn_list=[]
+    for name in enumFileExt(folder,'.txt'):
+        if name.find('event_DEM-')==0:
+            fn_list.append(name);
+    amp=10**n_amp;
+    for name in fn_list:
+        ratID=re.match('event_DEM-(\d{3})-',name).groups()[0];
+        event2lineTxt(folder+'/'+name,folder+'/'+ratID+'.txt',amp)
 
 if __name__=='__main__':
     print 'start'
     filename='R108-122911-spike.csv';
     t1=time.time()
     #for_plot(filename,' ',True);
-    #transAll2txt('PL/',5)
+    #transRec2txt('PL/',4)
+    #transRec2txt('IL/',4)
+    #transRec2txt('OFC/',4)
+    #transEvent('PFC_events/',4)
     print time.time()-t1
     print 'finish'
 
