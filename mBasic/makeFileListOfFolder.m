a6c6bf8d1748967d7b4c16f2f61b3533a908db37
yanxiangtianji <yanxiangtianji@gmail.com>
2015-09-10 03:54:29 -0400

    add event file listing function

--- mBasic/makeFileListOfFolder.m
+++ mBasic/makeFileListOfFolder.m
@@ -1,11 +1,16 @@
-function [fnlSpike,fnlBeh]=makeFileListOfFolder(pre)
+function [fnlSpike,fnlBeh,fnlEvn]=makeFileListOfFolder(pre)
   fnls=dir([pre,'/*-spike.txt']);
   fnlb=dir([pre,'/*-beh.txt']);
+  fnle=dir([pre,'/*-event.txt']);
   nFile=length(fnls);
   fnlSpike=cell(nFile,1);fnlBeh=cell(nFile,1);
   for i=1:nFile;
     fnlSpike(i)=[pre,'/',fnls(i).name];
     fnlBeh(i)=[pre,'/',fnlb(i).name];
   end;
-  
+  nFile=length(fnle);
+  fnlEvn=cell(nFile,1);
+  for i=1:nFile;
+    fnlEvn(i)=[pre,'/',fnle(i).name];
+  end
 end
