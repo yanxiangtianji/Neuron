d5f19d27dc5bd01e0b002a6a8ad7a89c2bf5afd8
yanxiangtianji <yanxiangtianji@gmail.com>
2015-09-10 03:52:35 -0400

    add neuron count for each file

--- mBasic/pickTrialFromFiles.m
+++ mBasic/pickTrialFromFiles.m
@@ -1,4 +1,4 @@
-function dataList=pickTrialFromFiles(fnListSpike,fnListBeh,cueIDs,trialRng,timeOffBeg=0,timeOffEnd=0,whetherRest=false)
+function [dataList,nNeuList]=pickTrialFromFiles(fnListSpike,fnListBeh,cueIDs,trialRng,timeOffBeg=0,timeOffEnd=0,whetherRest=false)
 %*cueIDs*: can be an integer or a vector.
 %*trialRng*:or a vector (as index used to all used cues);
 %           or a matrix (values at i-th column as index of i-th cue).
@@ -17,12 +17,14 @@ nTri=size(trialRng,1);
 
 nFile=numel(fnListSpike);
 dataList={};
+nNeuList=zeros(nFile,1);
 for i=1:nFile;
   %trialInfo=zeros(nTri,2,nCue);
   trialInfo=genTrialInfo(readCue(fnListBeh(i)),cueIDs,trialRng,whetherRest);
   trialInfo(:,1,:)+=timeOffBeg; trialInfo(:,2,:)+=timeOffEnd;
   t=pickTrial(readRaw(fnListSpike(i)),trialInfo);
   dataList=[dataList,t];
+  nNeuList(i)=size(t,2);
 end
 
 
