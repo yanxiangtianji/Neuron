e3f7bf519b9f5fac5542028dee14aa737994f270
yanxiangtianji <yanxiangtianji@gmail.com>
2015-09-10 03:49:15 -0400

    add read event function

---
+++ mBasic/readEvent.m
@@ -0,0 +1,18 @@
+function data=readEvent(fn)
+data=[];
+
+if(iscell(fn)) fn=cell2mat(fn); end;
+[fin,msg]=fopen(fn,'r');
+if(fin<0)
+  error(msg);
+end;
+
+t=fscanf(fin,'%f',2);
+while(!feof(fin))
+  data=[data;t'];
+  t=fscanf(fin,'%f',2);
+end
+fclose(fin);
+
+
+end
