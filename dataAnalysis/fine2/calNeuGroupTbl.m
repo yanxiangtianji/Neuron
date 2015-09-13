86270ebece6a6471fdcd578424c25fb027347305
yanxiangtianji <yanxiangtianji@gmail.com>
2015-09-13 03:23:49 -0400

    update logic of neuorn grouping function

--- dataAnalysis/fine2/calNeuGroupTbl.m
+++ dataAnalysis/fine2/calNeuGroupTbl.m
@@ -1,9 +1,12 @@
-function tbl=calNeuGroupTbl(nRat,sep,idx,neuAgg)
-  tbl=cell(nRat,3);
+function tbl=calNeuGroupTbl(nRat,idx,sepper,nNeuList)
+  nSep=length(sepper)+2;
+  tbl=cell(nRat,nSep);
+  neuSum=[0;cumsum(nNeuList)(:)];
+  sepper=[0;sepper(:);length(idx)];
   for i=1:nRat
-    t=(neuAgg(i)+1):neuAgg(i+1);
-    for j=1:length(sep)-1;%e.g. 1:negative, 2:nothing, 3:positive
-      tbl(i,j)=intersect(t,idx(sep(j)+1:sep(j+1)));
+    t=(neuSum(i)+1):neuSum(i+1);
+    for j=1:nSep-1;%e.g. 1:negative, 2:nothing, 3:positive
+      tbl(i,j)=intersect(t,idx(sepper(j)+1:sepper(j+1)));
     end
   end
 end
