969958e6e711716466516c793320486f90b4c455
yanxiangtianji <yanxiangtianji@gmail.com>
2015-09-15 01:58:03 -0400

    add a function for modified z-score, which use mean of elements in given range as baseline rather the mean value of all elements

---
+++ dataAnalysis/fine2/baselineZscore.m
@@ -0,0 +1,8 @@
+function z=baselineZscore(x,rngBL,dim=1)
+  y=selectRngByDim(x,rngBL,dim);
+  mu = mean (y, dim);
+  sigma = std (x, 0, dim);
+  s = sigma;
+  s(s==0) = 1;
+  z = bsxfun (@rdivide, bsxfun (@minus, x, mu), s);
+end
