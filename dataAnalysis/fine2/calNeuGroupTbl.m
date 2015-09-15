e18964d864801b06307b8bb76a4fe191f1067686
yanxiangtianji <yanxiangtianji@gmail.com>
2015-09-15 02:00:53 -0400

    fix bug for neuron grouping function

--- dataAnalysis/fine2/calNeuGroupTbl.m
+++ dataAnalysis/fine2/calNeuGroupTbl.m
@@ -1,11 +1,11 @@
 function tbl=calNeuGroupTbl(nRat,idx,sepper,nNeuList)
-  nSep=length(sepper)+2;
+  nSep=length(sepper)+1;
   tbl=cell(nRat,nSep);
   neuSum=[0;cumsum(nNeuList)(:)];
   sepper=[0;sepper(:);length(idx)];
   for i=1:nRat
     t=(neuSum(i)+1):neuSum(i+1);
-    for j=1:nSep-1;%e.g. 1:negative, 2:nothing, 3:positive
+    for j=1:nSep;%e.g. 1:negative, 2:nothing, 3:positive
       tbl(i,j)=intersect(t,idx(sepper(j)+1:sepper(j+1)));
     end
   end
