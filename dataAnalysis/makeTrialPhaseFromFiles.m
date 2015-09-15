6c858a79cd9836022b3024fd2781017598511199
yanxiangtianji <yanxiangtianji@gmail.com>
2015-09-15 02:02:22 -0400

    use input parameter to define the attitude to initial event for the bunch working function

--- dataAnalysis/makeTrialPhaseFromFiles.m
+++ dataAnalysis/makeTrialPhaseFromFiles.m
@@ -1,11 +1,11 @@
-function trialPhase=makeTrialPhaseFromFiles(fnlb,fnle,fmap,entPhase,nTri)
+function trialPhase=makeTrialPhaseFromFiles(fnlb,fnle,fmap,entPhase,nTri,mthd4initEvent='offset')
 
 nRat=length(fnlb);
 [nCue,nPha]=size(entPhase);
 trialPhase=zeros(nTri,nCue,nRat);%value is the number of phase in [0,nPha-1]
 for i=1:nRat;
   trialPhase(:,:,i)=makeTrialPhaseFromFile( ...
-    fnlb(i),fnle(fmap(i)),1:nCue,1:nTri,0,0,entPhase,'offset');
+    fnlb(i),fnle(fmap(i)),1:nCue,1:nTri,0,0,entPhase,mthd4initEvent);
 end
 
 end
