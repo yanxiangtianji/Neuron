058fa283b4cbb056d0d281d6f557367fa22b0a80
yanxiangtianji <yanxiangtianji@gmail.com>
2015-09-17 01:13:37 -0400

    update function calNeuGroupTbl by removing duplicated input parameter "nRat".

--- dataAnalysis/fine2/calNeuGroupTbl.m
+++ dataAnalysis/fine2/calNeuGroupTbl.m
@@ -1,10 +1,11 @@
-function tbl=calNeuGroupTbl(nRat,idx,sepper,nNeuList)
+function tbl=calNeuGroupTbl(nNeuList,idx,sepper)
+  nRat=length(nNeuList);
   nSep=length(sepper)+1;
   tbl=cell(nRat,nSep);
-  neuSum=[0;cumsum(nNeuList)(:)];
+  nNeuSum=[0;cumsum(nNeuList)(:)];
   sepper=[0;sepper(:);length(idx)];
   for i=1:nRat
-    t=(neuSum(i)+1):neuSum(i+1);
+    t=(nNeuSum(i)+1):nNeuSum(i+1);
     for j=1:nSep;%e.g. 1:negative, 2:nothing, 3:positive
       tbl(i,j)=intersect(t,idx(sepper(j)+1:sepper(j+1)));
     end
