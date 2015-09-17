52c266a9e463ae4777737e218e36463c9c4fa0cf
yanxiangtianji <yanxiangtianji@gmail.com>
2015-09-17 01:10:08 -0400

    update function condenseIntoPeriods to support both fixed period length and arbitrary range.

--- dataAnalysis/fine2/condenseIntoPeriods.m
+++ dataAnalysis/fine2/condenseIntoPeriods.m
@@ -1,10 +1,17 @@
-function res=condenseIntoPeriods(rtm,lenPeriod)
+function res=condenseIntoPeriods(rtm,sepperORinterval)
 %condense values in the first dimension into several periods (whose length is given).
-  sz=size(rtm); nBin=sz(1);% nNeu=sz(2);
-  nPer=ceil(nBin/lenPeriod);
+%sepper gives full range (both points, length>=2)
+  sz=size(rtm);% nBin=sz(1);% nNeu=sz(2);
+  nPer=length(sepperORinterval)-1;
+  if(nPer==0);%sepper is one scalar, which means fixed interval
+    sepper=[0;sepperORinterval:sepperORinterval:sz(2)];
+  else
+    sepper=sepperORinterval;
+    sepper(1)=max(0,sepper(1)-1);
+  end
   sz(1)=nPer;
   res=zeros(sz);
   for i=1:nPer;
-    res(i,:,:)=mean(rtm(1+(i-1)*lenPeriod:i*lenPeriod,:,:));
+    res(i,:,:)=mean(rtm(sepper(i)+1:sepper(i+1),:,:));
   end
 end
