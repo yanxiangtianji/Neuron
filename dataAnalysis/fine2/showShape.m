c08ac4df5562a14fd37fb5c406dd44af5bc6c653
yanxiangtianji <yanxiangtianji@gmail.com>
2015-09-18 03:52:26 -0400

    update showShape function to only one plot, add mean line

--- dataAnalysis/fine2/showShape.m
+++ dataAnalysis/fine2/showShape.m
@@ -1,19 +1,29 @@
-function showShape(rtm,nRow,nCol,nids,nNeuList, nTick,tickBeg,tickEnd, dashThre=0)
-  n=nRow*nCol;
-  if(n<length(nids))
-    error('More neurons (%d) than than can be display (%d)',length(nids),n)
-  end
+function showShape(rtm,nid,rlnId, nTick,tickBeg,tickEnd, dashThre=0,meanSepper)
+  _color='krgbmc';
   nBin=size(rtm,1);
-  rl=mapGNId2Local(nids,nNeuList);
-  c='krgbmc';
-  for i=1:min(n,length(nids));nid=nids(i);
-    subplot(nRow,nCol,i);
-    dash='';
-    if(mean(rtm(:,nid))<dashThre)
-      dash='--';
+  
+  cid=mod(rlnId(1),length(_color));
+  if(cid==0) cid=length(_color); end
+  
+  dash='';
+  if(mean(rtm(:,nid))<dashThre) dash='--'; end
+  
+  plot(rtm(:,nid),[_color(cid),dash]);
+  
+  if(exist('meanSepper','var'))
+    meanSepper=[0;meanSepper(:);nBin];
+    m=zeros(size(rtm,1),1);
+    for i=1:length(meanSepper)-1;
+      rng=meanSepper(i)+1:meanSepper(i+1); m(rng)=mean(rtm(rng,nid));
     end
-    plot(rtm(:,nid),[c(rl(i,1)),dash]);
-    setTimeX(nBin,nTick,tickBeg,tickEnd)
-    title(sprintf('N%d (R%d-N%d)',nid,rl(i,1),rl(i,2)))
+    c=_color(cid);
+    if(c!='k');  c='k';
+    else; c='r';  end;
+    hold on;
+    plot(m,[c,'--']);
+    hold off;
   end
+  
+  setTimeX(nBin,nTick,tickBeg,tickEnd)
+  title(sprintf('N%d (R%d-N%d)',nid,rlnId(1),rlnId(2)))
 end
